90aa24a1062e950d889a37fb88e8194ee9666afa
ksarangk <ksarangk@gmail.com>
2024-01-20 11:32:31 +0530

    SarangK_Assignment_1

---
+++ src/sarang_kulkarni/compareNum.java
@@ -0,0 +1,27 @@
+Class compareNum{
+	void greaterNum(int num1,int num2){
+		if(num1>num2){
+			sysyem.out.println(num1 + "is greater number between" +num1+ "and" +num2;
+			}
+		else{
+			sysyem.out.println(num2 + "is greater number between" +num1+ "and" +num2;
+			}
+		
+	}
+	
+	void smallerNum(int num1,int num2){
+		if(num1<num2){
+			sysyem.out.println(num1 + "is smaller number between" +num1+ "and" +num2;
+			}
+		else{
+			sysyem.out.println(num2 + "is smallar number between" +num1+ "and" +num2;
+			}
+	}
+	
+	public static void main(string[],agrs){
+		compareNum compareNum= new compareNum'();
+		compareNum.greaterNum(80,50);
+		compareNum.smallerNum(45,85);
+	}
+	
+}
