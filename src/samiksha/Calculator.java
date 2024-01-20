94685a4f518e9696003b5d57fbf3666b6feef9a9
Samiksha gadakh <samikshagadakh24@gmail.com>
2024-01-20 12:33:04 +0530

    Samiksha_Assignments_Till9

---
+++ src/samiksha/Calculator.java
@@ -0,0 +1,34 @@
+package samiksha;
+
+class Calculator{
+   int num1=10;
+   int num2=2;
+   
+   void add(){
+     int ans = num1+num2;
+	 System.out.println(ans);
+   }
+   
+   void sub(){
+     int ans= num1-num2;
+	 System.out.println(ans);
+   }
+   
+   void div(){
+     int ans= num1/num2;
+	 System.out.println(ans);
+   }
+   
+   void mul(){
+    int ans=num1*num2;
+	System.out.println(ans);
+   }
+   
+   public static void main(String[]args){
+    Calculator calculator=new Calculator();
+	calculator.add();
+	calculator.sub();
+	calculator.div();
+	calculator.mul();
+   }
+}
