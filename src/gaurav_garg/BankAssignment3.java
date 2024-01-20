e918daaedf4245dd856bde69f5e826f226895a72
Gaurav Garggit config --global user.name "Gaurav Garg <gauravgarg9136@gmail.com>
2024-01-20 11:15:05 +0530

    Gaurav_Assignment_1

---
+++ src/gaurav_garg/BankAssignment3.java
@@ -0,0 +1,34 @@
+package gaurav_garg;
+
+
+class BankAssignment3
+{
+	int balance = 1000;
+	
+	void debit(int n)
+	{
+		balance =balance -n;
+	}
+	void credit(int n)
+	{
+		balance=balance+n;
+	}
+	void display()
+	{
+		System.out.println("printbalance->" + balance);
+	}
+	
+	public static void main(String...s)
+	{
+		BankAssignment3 bankassignment3 =new BankAssignment3();
+		bankassignment3.debit(200);
+		bankassignment3.credit(500);
+		bankassignment3.credit(200);
+		bankassignment3.debit(700);
+		bankassignment3.display();
+	}
+	
+	
+	
+	
+}
