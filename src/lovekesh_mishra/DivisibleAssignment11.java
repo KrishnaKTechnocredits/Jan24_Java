61d9ac3aee195dc09c3dcb1070f55c789c01f67c
Lovekesh mishra <Lovekesh mishra>
2024-01-21 20:01:59 +0530

    LovekeshM_Assignment_11

--- src/lovekesh_mishra/DivisibleAssignment11.java
+++ src/lovekesh_mishra/DivisibleAssignment11.java
@@ -1,18 +1,17 @@
 package lovekesh_mishra;
 
-class DivisibleAssignment11{
+class DivisibleAssignment11 {
 
-                 void divisible(int startindex, int endindex){
-                        for(int num=startindex; num<=endindex; num++){
-                           if(num%5==0 && num%3==0){
-                            System.out.println("Divisible number are--" + num);
-                        }
-                   }
-               }
+	void divisible(int startindex, int endindex) {
+		for (int num = startindex; num <= endindex; num++) {
+			if (num % 5 == 0 && num % 3 == 0) {
+				System.out.println("Divisible number are--" + num);
+			}
+		}
+	}
 
-                  public static void main(String[] args){
-                   DivisibleAssignment11 divisibleAssignment11 = new DivisibleAssignment11();
-                      divisibleAssignment11.divisible(5, 40);
-               }
+	public static void main(String[] args) {
+		DivisibleAssignment11 divisibleAssignment11 = new DivisibleAssignment11();
+		divisibleAssignment11.divisible(5, 40);
+	}
 }
-                   
