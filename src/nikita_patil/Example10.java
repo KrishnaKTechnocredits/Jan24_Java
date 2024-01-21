a05ac1d7f8ee0319129fe169aeaa0dcc9a025c22
Nikita Patil <nikipatil1920@gmail.com>
2024-01-21 20:25:50 +0530

    Example

--- src/nikita_patil/Example10.java
+++ src/nikita_patil/Example10.java
@@ -1,28 +1,30 @@
-class Example10{
-       int age = 1;
-	   String name = "Nikita";
-	   
-	   void setData(int age, String name){
-	 
-			this.age = age;
-			this.name=name;
-		}
-		 
-		void updateData(String name){
-		    this.name = name;
-		}
-			
-		void display(){
-			System.out.println(age + "-------> "+ name);
-		}
-			
-		public static void main(String[] args){
-			   Example10 e1 = new Example10();
-			   e1.setData(19, "Jaydeep");
-			   e1.display();
-			   
-			   Example10 e2 = new Example10();
-			   e2.updateData("Niki");
-			   e2.display();
-			   }
-		}
+package nikita_patil;
+
+class Example10 {
+	int age = 1;
+	String name = "Nikita";
+
+	void setData(int age, String name) {
+
+		this.age = age;
+		this.name = name;
+	}
+
+	void updateData(String name) {
+		this.name = name;
+	}
+
+	void display() {
+		System.out.println(age + "-------> " + name);
+	}
+
+	public static void main(String[] args) {
+		Example10 e1 = new Example10();
+		e1.setData(19, "Anki");
+		e1.display();
+
+		Example10 e2 = new Example10();
+		e2.updateData("Niki");
+		e2.display();
+	}
+}
