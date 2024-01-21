5fffeb74a7fab9f24520108f784110986fd26881
Prathamesh Tati <tati.pratham@gmail.com>
2024-01-21 17:54:46 +0530

    updated program

--- src/prathamesh_tati/DivisibleNumbers.java
+++ src/prathamesh_tati/DivisibleNumbers.java
@@ -1,3 +1,11 @@
+/*Assignment - 10 : 21st Jan'2024
+1. On user defined range print all numbers which are divisible by 5 and divisible by 3.
+Hint : pass start and end index as a parameter.
+Input : Range -> 5 to 40
+Output : Divisible by 5 & 3, numbers are:
+15
+30*/
+
 package prathamesh_tati;
 
 public class DivisibleNumbers {
