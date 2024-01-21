52a060db18177dac3a64a2149b753b2d258c4ca2
Prathamesh Tati <tati.pratham@gmail.com>
2024-01-21 19:11:12 +0530

    removed commented part

--- src/prathamesh_tati/ReverseOrderNumberDivision.java
+++ src/prathamesh_tati/ReverseOrderNumberDivision.java
@@ -1,16 +1,5 @@
 /* Assignment - 11 : 21st Jan'2024 
- * On user defined range print all numbers which are divisible by 7 or 13 in reverse order.
- * Input : Range -> 5 to 40
- * 
- * Output : Divisible by 7 or 13, numbers are:
- * 39 is divisible by 13
- * 35 is divisible by 7
- * 28 is divisible by 7
- * 26 is divisible by 13
- * 21 is divisible by 7
- * 14 is divisible by 7
- * 13 is divisible by 13
- * 7 is divisible by 7 */
+ * On user defined range print all numbers which are divisible by 7 or 13 in reverse order. */
 
 package prathamesh_tati;
 
