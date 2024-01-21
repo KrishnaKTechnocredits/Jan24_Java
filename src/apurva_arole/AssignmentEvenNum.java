1a605b9447dd425d2a907498a18282978ea44aee
ApurvaArole <apurvaarole67gmail.com>
2024-01-21 18:28:39 +0530

    ApurvaA_Assignment_10

--- src/apurva_arole/AssignmentEvenNum.java
+++ src/apurva_arole/AssignmentEvenNum.java
@@ -4,9 +4,10 @@ public class AssignmentEvenNum{
 	
     void PrintNum(int StartIndex, int EndIndex){
      for(int Index=StartIndex;Index<=EndIndex;Index++){
-    	 if(Index%2==0)
+    	 if(Index%2==0) {
     		System.out.println("Even number is=" +Index);
              }
+         }
     }   	   
     public static void main(String[]args){
         	AssignmentEvenNum assignmentEvenNum= new AssignmentEvenNum();
