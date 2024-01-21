cdf91ae86b60ccce7eb38f81f61f5ead2ce7504e
Ankita Sambhare <ankitasambhare94@gmail>
2024-01-21 22:35:05 +0530

    Ankita_Assignement_10

--- src/ankita_s/EvenNumbers.java
+++ src/ankita_s/EvenNumbers.java
@@ -3,14 +3,11 @@ package ankita_s;
 public class EvenNumbers {
 
 	void printEvenNumbers(int startindex, int endindex) {
-
 		for (int num = startindex; num <= endindex; num++) {
 			if (num % 2 == 0) {
-
 				System.out.println(num);
 			}
 		}
-
 	}
 
 	public static void main(String[] agrs) {
@@ -18,5 +15,4 @@ public class EvenNumbers {
 		System.out.println("Below are the even numbers between 10 to 15");
 		evennumbers.printEvenNumbers(10, 15);
 	}
-
 }
