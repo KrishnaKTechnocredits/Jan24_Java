ce2aa63e06974d287c37c663fb914aa4a183fdf3
Ankita Sambhare <ankitasambhare94@gmail>
2024-01-22 02:20:04 +0530

    AnkitaS_Assignment_16

--- src/ankita_s/SumAndAvgDivisiblebyFiveandSeven.java
+++ src/ankita_s/SumAndAvgDivisiblebyFiveandSeven.java
@@ -1,28 +1,26 @@
 package ankita_s;
 
 public class SumAndAvgDivisiblebyFiveandSeven {
-int sum=0;
-int count=0;
-int avg=0;
-int num=0;
-	void printSumAndAvgDivisiblebyFiveandSeven(int startindex,int endindex) {
-		
-		for (num = startindex; num <= endindex; num++){
-			if (num % 5 == 0 && num % 7 == 0){
+	int sum = 0;
+	int count = 0;
+	int avg = 0;
+	int num = 0;
+
+	void printSumAndAvgDivisiblebyFiveandSeven(int startindex, int endindex) {
+		for (num = startindex; num <= endindex; num++) {
+			if (num % 5 == 0 && num % 7 == 0) {
 				System.out.println("Numbers are: " + num);
 				count++;
 				sum = sum + num;
 			}
 		}
-		avg = sum/count;
-		
-		System.out.println("Sum= "+ sum);
-		System.out.println("Average= "+ avg);
-
+		avg = sum / count;
+		System.out.println("Sum= " + sum);
+		System.out.println("Average= " + avg);
 	}
 
 	public static void main(String[] args) {
-		SumAndAvgDivisiblebyFiveandSeven sumAndAvgDivisiblebyFiveandSeven=new SumAndAvgDivisiblebyFiveandSeven();
+		SumAndAvgDivisiblebyFiveandSeven sumAndAvgDivisiblebyFiveandSeven = new SumAndAvgDivisiblebyFiveandSeven();
 		System.out.println("Found 3 numbers which are divible by 5 & 7:-");
 		sumAndAvgDivisiblebyFiveandSeven.printSumAndAvgDivisiblebyFiveandSeven(1, 110);
 	}
