2161e4d3cafa5acd762cc1dded2d616a4873b2b3
Shridhar Kumbhar <shridhartechno@gmail.com>
2024-01-21 23:19:31 +0530

    Shridhar_Assignment_9B

---
+++ src/shridhar_k/Assignment9B.java
@@ -0,0 +1,36 @@
+/*
+ * Write only one program having the following methods.
+
+1. print all even numbers in a user defined range.
+
+Hint : pass start and end index as a parameter.
+
+Input : Range -> 10 to 15
+
+Output : Even numbers are:
+
+10
+
+12
+
+14
+ */
+
+package shridhar_k;
+
+public class Assignment9B {
+
+	void printEvenNumber(int startNum, int endNum) {
+		System.out.println("Even numbers between" + startNum + " and " + endNum +  " are: ");
+		for (int index = startNum; index <= endNum; index++) {
+			if(index % 2 == 0) {
+				System.out.println(index);
+			}
+		}
+	}
+	
+	 public static void main (String [] arg) {
+		Assignment9B assignment9b = new Assignment9B();
+		assignment9b.printEvenNumber(10, 15);
+	}
+}
