191a762fffff38fdf0c65c21e326e8207df5b97b
sasmita sahu <sasmitasahu2119@gmail.com>
2024-01-22 17:35:46 +0530

    sasmita_Assignment_14

---
+++ src/sasmita/DivisiableRange.java
@@ -0,0 +1,22 @@
+package sasmita;
+
+public class DivisiableRange {
+	
+	void printDivisiable(int startRange, int endRange){
+		int count=1;
+		for(int i=startRange; i<= endRange; i++) {
+			if(i%3 == 0 && i%4 == 0) {
+				if (count <= 3) {
+				 System.out.println(i);
+				 count++;
+			}
+			else
+				break;
+		}
+		}
+	}
+	public static void main(String[]args){
+		DivisiableRange div = new DivisiableRange();
+		div.printDivisiable(10,500);
+	}
+}
