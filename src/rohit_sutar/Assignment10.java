7dab36ed0ad039ecd2df55156f60004467b18b23
Rohit Sutar <rrsutar1995@gmail.com>
2024-01-22 21:44:06 +0530

    showing this message hence comminting Your local changes to the following files would be overwritten by checkout:

--- src/rohit_sutar/Assignment10.java
+++ src/rohit_sutar/Assignment10.java
@@ -1,23 +1,20 @@
 package rohit_sutar;
 
-/*
- * Program is to print all even numbers between 10 -15
- * Output will be 10, 12, 14
- */
-
 public class Assignment10 {
 
-	void printAllEvenNumbers(int startIndex, int endIndex) {
-		System.out.println("Below listed numbers are Even numbers between "+startIndex+ " and "+endIndex);
-		for(int i = startIndex ; i<= endIndex ; i++) {
+	void printEvenNumber(int startIndex, int endIndex) {
+		System.out.println("Below numbers are even number between range of "+startIndex+" and "+endIndex);
+		for(int i = startIndex ; i<=endIndex ; i++) {
 			if(i%2==0) {
 				System.out.println(i);
 			}
 		}
 	}
 	
-	public static void main(String args[]) {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Assignment10 assignment10 = new Assignment10();
-		assignment10.printAllEvenNumbers(10, 15);
+		assignment10.printEvenNumber(10, 15);
 	}
+
 }
