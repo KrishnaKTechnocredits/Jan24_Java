f8fecd3614930c9c84f4b224815e6f9cf4267ef1
Shramika vaidya <shramikavaidya17@gmail.com>
2024-01-22 08:23:38 +0530

    Shramika_Assignment10

---
+++ src/shramika_vaidya/Numbers.java
@@ -0,0 +1,14 @@
+package shramika_vaidya;
+
+class Numbers{
+ 
+	void printEvenNumbers(int startRange,int endRange){
+		for(int num=startRange; num<=endRange; num=num+2){
+			System.out.println(num);
+		}
+	}
+	public static void main(String [] args){
+		Numbers numbers = new Numbers();
+		numbers.printEvenNumbers(10,15);
+	}
+}
