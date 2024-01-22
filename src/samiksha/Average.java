e216db494860e5b22516c12203b6e85fa821a02e
Samiksha gadakh <samikshagadakh24@gmail.com>
2024-01-22 22:14:16 +0530

    Samiksha_Assignment_16

--- src/samiksha/Average.java
+++ src/samiksha/Average.java
@@ -6,19 +6,20 @@ public class Average {
 	int count;
 	
 	void printSumAndAvg(int startR, int endR) {
-	    for(int num = startR; num <= endR; num++) {
+	   for(int num = startR; num <= endR; num++) {
 	        if(num % 5 == 0 && num % 7 == 0){
 	    	   sum = sum + num;
 	    	   count++;
 	        }
-	    }
-		int avg = sum/count;
-		System.out.println("sum is " + sum);
-		System.out.println("avg is " + avg);
+	   }
+	   int avg = sum/count;
+	   System.out.println("sum is " + sum);
+	   System.out.println("avg is " + avg);
 	}
 	
 	public static void main(String[] args) {
-		Average average = new Average();
-		average.printSumAndAvg(1, 110);
+	   Average average = new Average();
+	   average.printSumAndAvg(1, 110);
 	}
 }
+ 
