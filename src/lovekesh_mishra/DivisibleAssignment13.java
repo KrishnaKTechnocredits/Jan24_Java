f61f39a590b1c40c2fb2d59cf4965cae44b455ef
Lovekesh mishra <Lovekesh mishra>
2024-01-22 20:11:53 +0530

    LovekeshM_Assigment_13

--- src/lovekesh_mishra/DivisibleAssignment13.java
+++ src/lovekesh_mishra/DivisibleAssignment13.java
@@ -1,19 +1,19 @@
 package lovekesh_mishra;
 
-class DivisibleAssignment13{
-                   
-                   void lastNumber(int startindex, int endindex){
-                           int count=0;
-                        for(int num=startindex; num<=endindex; num++){
-                           if(num%5==0 && num%9==0){
-                               count= num;
-                   }
-                }
-                      System.out.println("last number divisible by 5 & 9 is---->" + count);
-               }
+class DivisibleAssignment13 {
 
-                    public static void main(String[] args){
-                       DivisibleAssignment13 divisibleAssignment13 = new DivisibleAssignment13();
-                            divisibleAssignment13.lastNumber(5, 100);
-                    }
+	void lastNumber(int startindex, int endindex) {
+		int count = 0;
+		for (int num = startindex; num <= endindex; num++) {
+			if (num % 5 == 0 && num % 9 == 0) {
+				count = num;
+			}
+		}
+		System.out.println("last number divisible by 5 & 9 is---->" + count);
+	}
+
+	public static void main(String[] args) {
+		DivisibleAssignment13 divisibleAssignment13 = new DivisibleAssignment13();
+		divisibleAssignment13.lastNumber(5, 100);
+	}
 }
