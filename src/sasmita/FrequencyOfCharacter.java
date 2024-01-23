23e00e5c91fc68012c54e094a7bf5add888a92ff
sasmita sahu <sasmitasahu2119@gmail.com>
2024-01-23 12:37:13 +0530

    src/sasmita/FrequencyOfCharacter.java

---
+++ src/sasmita/FrequencyOfCharacter.java
@@ -0,0 +1,19 @@
+package sasmita;
+
+public class FrequencyOfCharacter{
+	int count;
+	void frequency(String input,char t) {
+		for(int index=0; index<input.length(); index++){
+			//System.out.println(input.charAt(index));//
+			char ch =input.charAt(index);
+			if(t == ch) {
+				count++;	
+			}
+		}
+		System.out.print("Frequency of " +t+" in technocredits is " +count);
+	}
+	public static void main(String[]args) {
+		FrequencyOfCharacter freq = new FrequencyOfCharacter();
+		freq.frequency("technocredits", 't');
+	}
+}
