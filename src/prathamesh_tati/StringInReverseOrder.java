4f5355f2d036b49f2e6313e147b0e3e18c2cb134
Prathamesh Tati <tati.pratham@gmail.com>
2024-01-23 23:51:31 +0530

    PrathameshT_Assignment_18

---
+++ src/prathamesh_tati/StringInReverseOrder.java
@@ -0,0 +1,24 @@
+/* Assignment - 18 : 23th Jan'2024 
+ Print character of given String in reverse order.*/
+
+package prathamesh_tati;
+
+public class StringInReverseOrder {
+
+	void reverseOrderString(String str) {
+		for (int index = str.length() - 1; index >= 0; index--) {
+			System.out.println(str.charAt(index));
+
+		}
+	}
+
+	public static void main(String[] args) {
+		StringInReverseOrder stringInReverseOrder = new StringInReverseOrder();
+		stringInReverseOrder.reverseOrderString("techno");
+	}
+}
+
+/*
+ * 
+ * input : techno output : o n h c e t
+ */
