d627645c128122bcc34974e78d927dc798cae0e8
Shafaque Ali <shafaqueali18@gmail.com>
2024-01-23 21:16:25 +0530

    ShafaqueA_Assignment17

--- src/shafaque/CharFrequency.java
+++ src/shafaque/CharFrequency.java
@@ -1,23 +1,21 @@
 package shafaque;
 
 public class CharFrequency {
-	
-	int count=0;
-	
-	void checkcharfreq(String input, char targetchar)
-	{
-		for(int index =0; index < input.length(); index++)
-		{
+
+	int count = 0;
+
+	void checkcharfreq(String input, char targetchar) {
+		for (int index = 0; index < input.length(); index++) {
 			char ch = input.charAt(index);
-			if(ch == targetchar) {
+			if (ch == targetchar) {
 				count++;
 			}
 		}
-		System.out.println("Frequency of t in technocredits is  " +count);
+		System.out.println("Frequency of t in technocredits is  " + count);
 	}
-public static void main(String[] args)
-{
-	CharFrequency charFrequency = new CharFrequency();
-	charFrequency.checkcharfreq("technocredits" , 't');
-}
+
+	public static void main(String[] args) {
+		CharFrequency charFrequency = new CharFrequency();
+		charFrequency.checkcharfreq("technocredits", 't');
+	}
 }
