ff1f1aec3986456b1b30f3877b60c7995e77dc81
Ankita Sambhare <ankitasambhare94@gmail>
2024-01-24 00:05:01 +0530

    AnkitaS_Assignment_17

---
+++ src/ankita_s/Frequencyofcharacter.java
@@ -0,0 +1,19 @@
+package ankita_s;
+
+public class Frequencyofcharacter {
+
+	void charfreq(String input, char target) {
+		int count = 0;
+		for (int index = 0; index < input.length(); index++) {
+			char x = input.charAt(index);
+			if (x == 't')
+				count++;
+		}
+		System.out.println(" frequency of t in technocredits is  " + count);
+	}
+
+	public static void main(String[] args) {
+		Frequencyofcharacter frequencystr = new Frequencyofcharacter();
+		frequencystr.charfreq("techocredits", 't');
+	}
+}
