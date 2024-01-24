6e6851aad82b054e73061624a41ec6a3e8a995b1
sasmita sahu <sasmitasahu2119@gmail.com>
2024-01-24 18:52:31 +0530

    sasmita_Assignment_19

---
+++ src/sasmita/FrequencyOfCharLower.java
@@ -0,0 +1,21 @@
+package sasmita;
+
+public class FrequencyOfCharLower {
+	
+	void frequencyofchar(String str,char ch) {
+		int count=0;
+		str = str.toLowerCase();
+		for(int index=0; index<str.length(); index++){
+			char newchar = str.charAt(index);
+			if(newchar == ch) {
+				count++;
+			}
+		}
+		System.out.println("Frequency of " + ch +" in technocrediTs teaches Technology is " +count);
+	}
+	public static void main(String[]args) {
+		FrequencyOfCharLower freq= new FrequencyOfCharLower();
+		freq.frequencyofchar("technocrediTs teaches Technology",'t');
+	}
+
+}
