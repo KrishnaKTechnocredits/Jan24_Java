742b55ddc7057f5b8ef4e24a32ce17d03a54960a
Shafaque Ali <shafaqueali18@gmail.com>
2024-01-24 22:03:14 +0530

    ShafaqueA_Assignment19

--- src/shafaque/Assignment19.java
+++ src/shafaque/Assignment19.java
@@ -1,31 +1,25 @@
 package shafaque;
 
 public class Assignment19 {
-	
-	int count= 0;
-	
-	void casesensitive(String str ,char targetchar)
-	   {
-		    str = str.toLowerCase(); 
-	   
-	   for (int index = 0; index < str.length(); index++)
-	  {
-		   				
-				if (targetchar == str.charAt(index)) 
-				{
-					count++;
-				}
-			}
-			System.out.println("Frequency of t in technocredits is  " + count);
-			
-		}
-		   
 
-		public static void main(String[] args) 
-		{
-			Assignment19 assignment19 = new Assignment19();
-			assignment19.casesensitive("technocrediTs teaches Technology", 't');
-			
+	int count = 0;
+
+	void casesensitive(String str, char targetchar) {
+		str = str.toLowerCase();
+
+		for (int index = 0; index < str.length(); index++) {
+
+			if (targetchar == str.charAt(index)) {
+				count++;
+			}
 		}
+		System.out.println("Frequency of t in technocredits is  " + count);
+
 	}
 
+	public static void main(String[] args) {
+		Assignment19 assignment19 = new Assignment19();
+		assignment19.casesensitive("technocrediTs teaches Technology", 't');
+
+	}
+}
