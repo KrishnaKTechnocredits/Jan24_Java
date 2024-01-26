603efda9786110163677d924fdc834508ec3856f
Puja Poreddiwar <pujaporeddiwar123@gmail.com>
2024-01-26 09:55:44 +0530

    Puja_Assignment_20

--- src/puja_poreddiwar/FindEvenNumber.java
+++ src/puja_poreddiwar/FindEvenNumber.java
@@ -3,40 +3,48 @@ package puja_poreddiwar;
 public class FindEvenNumber {
 
 	void forloopEvenNo(int startRange, int endRange) {
+		int count = 0;
 		System.out.println("For_loop - first 5 Even numbers are: ");
 		for (int num = startRange; num <= endRange; num++) {
 			if (num % 2 == 0) {
 				System.out.println(num);
+				count++;
+			}
+			if (count == 5) {
+				break;
 			}
 		}
 	}
 
 	void whileloopEvenNo() {
 		System.out.println("While_loop- first 5 Even numbers are: ");
-		int num =2;
-		while(num<=10) {
-			System.out.println(num);
-			num=num+2;
+		int count = 0;
+		int num = 2;
+		while (count < 5) {
+			if (num % 2 == 0) {
+				System.out.println(num);
+				count++;
+			}
+			num++;
 		}
-		
 	}
 
 	void dowhileloopEvenNo(int input) {
 		System.out.println("do_while_loop- first 5 Even numbers are: ");
 		int count = 0;
 		int num = 1;
-		do{
-			if(num % 2 == 0) {
+		do {
+			if (num % 2 == 0) {
 				System.out.println(num);
 				count++;
 			}
 			num++;
-		}while(count < input);
+		} while (count < input);
 	}
-	
+
 	public static void main(String[] args) {
 		FindEvenNumber findEvenNumber = new FindEvenNumber();
-		findEvenNumber.forloopEvenNo(1, 10);
+		findEvenNumber.forloopEvenNo(1, 20);
 		System.out.println("-----------------------------");
 		findEvenNumber.whileloopEvenNo();
 		System.out.println("-----------------------------");
