35ff0a94b61bcc966a1035d4c1cd6647894bf807
Krishna Kanani <kananikrishna427@gmail.com>
2024-01-27 13:25:31 +0530

    changes done as requested

--- src/technocredits/DataType.java
+++ src/technocredits/DataType.java
@@ -11,9 +11,6 @@ public class DataType {
 	
 	char ch = 'a'; // 2 bytes [16 bits]
 	boolean flag = true; // 1 byte [8 bits]
-	
 	float f = 100.12f; // 4 byte [32 bits] 
 	double d = 10.20034;// 8 byte [64 bits]
-	
-	
 }
