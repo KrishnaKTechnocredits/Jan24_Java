8ea6183571cf5a9ebcdf51d0cf7217e3cb2ba2eb
Krishna Kanani <kananikrishna427@gmail.com>
2024-01-27 13:24:21 +0530

    Static Non static examples

---
+++ src/technocredits/DataType.java
@@ -0,0 +1,19 @@
+package technocredits;
+
+public class DataType {
+	
+	String str = "Hi";
+	
+	byte b = 127; // 1 byte [8 bits] 
+	short s = 32767; // 2 bytes [16 bits]
+	int i = 10; // 4 bytes [1 byte = 8 bits, 32 bits]
+	long l = 10; // 8 byte [64 bits]
+	
+	char ch = 'a'; // 2 bytes [16 bits]
+	boolean flag = true; // 1 byte [8 bits]
+	
+	float f = 100.12f; // 4 byte [32 bits] 
+	double d = 10.20034;// 8 byte [64 bits]
+	
+	
+}
---
+++ src/technocredits/StaticNonStaticEx2.java
@@ -0,0 +1,18 @@
+package technocredits;
+
+public class StaticNonStaticEx2 {
+
+	void m1() {
+		m2();
+	}
+	
+	static void m2() {
+		
+	}
+	
+	
+	public static void main(String[] args){
+		System.out.println("Hello");
+		
+	}
+}
