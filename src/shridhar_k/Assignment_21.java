9e16882ab8d6b5654e219127d1f85c56e9e0ad4c
Shridhar Kumbhar <shridhartechno@gmail.com>
2024-01-27 23:12:23 +0530

    ShridharK_Assignment_21

---
+++ src/shridhar_k/Assignment_21.java
@@ -0,0 +1,25 @@
+//Find out the minimum numbers required to create sum 150, consider only those numbers which is divisible by 6.
+package shridhar_k;
+
+public class Assignment_21 {
+	int counter = 0;
+
+	void sumOFNum(int reqSum, int divNum) {
+		int counter = 0;
+		int num = 1;
+		int sum = 0;
+		while (sum <= reqSum) {
+			if (num % divNum == 0) {
+				sum = sum + num;
+				counter++;
+			}
+			num++;
+		}
+		System.out.println("output : " + counter);
+	}
+
+	public static void main(String[] arg) {
+		Assignment_21 assignment_21 = new Assignment_21();
+		assignment_21.sumOFNum(150, 6);
+	}
+}
