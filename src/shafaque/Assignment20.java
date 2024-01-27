573d5d7c6c240f842857720b4ab287f1a6c9fb74
Shafaque Ali <shafaqueali18@gmail.com>
2024-01-27 21:27:57 +0530

    ShafaqueA_Assignment20

--- src/shafaque/Assignment20.java
+++ src/shafaque/Assignment20.java
@@ -1,29 +1,44 @@
 package shafaque;
 
 public class Assignment20 {
-	void forloop(int num1, int num2) {
-		for (int i = num1; i <= num2; i++)
-			if (i % 2 == 0)
+	void forloop(int num1, int num2) 
+	{
+		int count = 0;
+		for (int i = num1; i < num2; i++)
+		{
+			if (i % 2 == 0) {
 				System.out.println(i);
+		        count++;
+			}
+		        if(count == 5)
+		        break;
 	}
+	}
+	
 
-	void whileloop(int num1, int num2) {
-		int i = num1;
-		while (i <= num2) {
-			if (i % 2 == 0)
-				System.out.println(i);
-			i++;
+	void whileloop() {
+		int num = 0;
+		int count = 0;
+		while (count < 5) {
+			if (num % 2 == 0) {
+				System.out.println(num);
+			count++;
+		}
+		num++;
 		}
 	}
 
-	void doWhileloop(int num1, int num2) {
-		int i = num1;
+	void doWhileloop() {
+		int num = 0;
+		int count=0;
 		do {
-             if (i % 2 == 0)
-				System.out.println(i);
-			i++;
+             if (num % 2 == 0) {
+				System.out.println(num);
+			count++;
 		}
-		while (i <= num2);
+		num++;
+		}
+		while (count < 5);
 	}
 
 	public static void main(String[] args) {
@@ -31,9 +46,9 @@ public class Assignment20 {
 		System.out.println("For loop");
 		assignment20.forloop(10, 20);
 		System.out.println("While loop");
-		assignment20.whileloop(10, 20);
+		assignment20.whileloop();
 		System.out.println("Do While loop");
-		assignment20.doWhileloop(10, 20);
+		assignment20.doWhileloop();
 	}
 
 }
