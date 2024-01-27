55f6ecdfcbdc6856fcb5bf1def4cecf5b2dc33bb
Akshay Gaikwad <akshaygaikwad1218@gmail.com>
2024-01-27 22:25:00 +0530

    AkshayG_Assignment_21

---
+++ src/akshay_gaikwad/MinNumGetSum.java
@@ -0,0 +1,23 @@
+package akshay_gaikwad;
+
+public class MinNumGetSum {
+	
+	void minNumGetSum(int input) {
+		int count = 0;
+		int sum = 0;
+		int num = 1;
+		while(sum < input) {
+			if (num % 6 == 0) {
+				sum = sum + num;
+				count++;
+			}
+			num++;
+		}
+		System.out.println("Minimun Numbers required to get sum " + input + " which are divisble by 6 are : " + count);
+	}
+	public static void main(String[] args) {
+		MinNumGetSum minnumgetsum = new MinNumGetSum();
+		minnumgetsum.minNumGetSum(150);
+	}
+
+}
