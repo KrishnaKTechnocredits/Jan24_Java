22fc839b9bcb9d0386ad9515d7ea14d78e10d5dc
ksarangk <ksarangk@gmail.com>
2024-01-28 14:41:26 +0530

    Code Change

--- src/sarang_kulkarni/CountDigits.java
+++ src/sarang_kulkarni/CountDigits.java
@@ -10,7 +10,7 @@ public class CountDigits {
 	
 	static int count=0;
 	void coundigits() {
-		String str="te3c3hn5o2cre3dits4";
+		String str="te3c3hn5o2cre3dits";
 		for(int i=0;i<str.length();i++) {
 			char ch=str.charAt(i);
 			if(Character.isDigit(ch)) {
