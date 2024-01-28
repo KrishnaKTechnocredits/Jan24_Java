9b4d567cb5fe57dbdb2713fe202933ecf13999be
Hiral Purohit <rajpurohithiral@gmail.com>
2024-01-28 14:23:21 +0530

    Hiral_Assignment_21

--- src/hiral_purohit/WhileLoop.java
+++ src/hiral_purohit/WhileLoop.java
@@ -1,23 +1,22 @@
 package hiral_purohit;
-//Find out the minimum numbers required to create sum 150, 
-//consider only those numbers which is divisible by 6.
 
 public class WhileLoop {
-	int count =0;
-	
-	void m1(){
-		int index =1;
+	int count = 0;
+
+	void m1() {
+		int index = 1;
 		int sum = 0;
 		while (sum <= 150) {
-			if (index % 6 ==0) {
+			if (index % 6 == 0) {
 				sum = sum + index;
 				count++;
-		}index++;
+			}
+			index++;
 		}
 		System.out.println("output is " + count);
 	}
-	
-	public static void main( String[] args) {
+
+	public static void main(String[] args) {
 		WhileLoop whileLoop = new WhileLoop();
 		whileLoop.m1();
 	}
