2c072680f8fdc625ef95ae4363f65e286f902c3a
Kanchan Ghuge <kg5995976@gmail.com>
2024-01-29 21:51:01 +0530

    KanchanG_Assignment_2

--- src/kanchan_ghuge/CalculatorTricky.java
+++ src/kanchan_ghuge/CalculatorTricky.java
@@ -1,41 +1,40 @@
 package kanchan_ghuge;
 
-class CalculatorTricky{
-	
+class CalculatorTricky {
+
 	int answer;
-	
-	void add(int num1, int num2){
-		answer = answer + ( num1 + num2);
+
+	void add(int num1, int num2) {
+		answer = answer + (num1 + num2);
 		System.out.println("Addition = " + answer);
 	}
-	
-	void sub(int num1, int num2){
-		answer = answer + ( num1 - num2);
+
+	void sub(int num1, int num2) {
+		answer = answer + (num1 - num2);
 		System.out.println("Substraction = " + answer);
 	}
-	
-	void mul(int num1, int num2){
-		answer = answer + (num1 *  num2);
+
+	void mul(int num1, int num2) {
+		answer = answer + (num1 * num2);
 		System.out.println("Multiplication = " + answer);
 	}
 
-    void div(int num1, int num2){
+	void div(int num1, int num2) {
 		answer = answer + (num1 / num2);
 		System.out.println("Division = " + answer);
 	}
-	
-	void display(){
+
+	void display() {
 		System.out.println("Final Answer = " + answer);
 	}
-	
-	public static void main(String [] args){
+
+	public static void main(String[] args) {
 		CalculatorTricky calculatorTricky = new CalculatorTricky();
-		calculatorTricky.add(10,20);
-		calculatorTricky.sub(20,4);
-		calculatorTricky.mul(10,5);
-		calculatorTricky.div(10,2);
+		calculatorTricky.add(10, 20);
+		calculatorTricky.sub(20, 4);
+		calculatorTricky.mul(10, 5);
+		calculatorTricky.div(10, 2);
 		calculatorTricky.display();
 	}
-}	
-
-		
+}
+	
