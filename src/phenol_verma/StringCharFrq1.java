07c5c7601433acdffb92a5c3ff7e9f5c45553540
PhenolVerma <phenol.verma@gmail.com>
2024-01-29 11:06:49 +0530

    PhenolV_Assignment_26

--- src/phenol_verma/StringCharFrq1.java
+++ src/phenol_verma/StringCharFrq1.java
@@ -11,10 +11,17 @@ public class StringCharFrq1 {
 
 	void charCount(String str) {
 		for (int index = 0; index < str.length(); index++) {
+			int count = 0;
 			char chr = str.charAt(index);
+			for (int i = 0; i < str.length(); i++) {
+				if (str.charAt(i) == chr) {
+					count++;				}	
+			}
+			if (count == 1) {
+				System.out.print(str.charAt(index));
+			}
 			
 		}
-				
 	}
 	
 	public static void main(String[] args) {
