1a9b18741b3a4702e5265236bbbf703540646dc3
Prathamesh Tati <tati.pratham@gmail.com>
2024-01-30 01:25:01 +0530

    PrathameshT_Assignemnt_25

---
+++ src/prathamesh_tati/GetDuplicateChars.java
@@ -0,0 +1,19 @@
+package prathamesh_tati;
+
+public class GetDuplicateChars {
+	int count = 0;
+
+	void getDuplicateCharacters(String str) {
+		for (int index = 0; index <= str.length() - 1; index++) {
+			char ch = str.charAt(index);
+			if (index == str.indexOf(ch))
+				if (str.indexOf(ch) != str.lastIndexOf(ch))
+					System.out.print(ch);
+		}
+	}
+
+	public static void main(String[] args) {
+		GetDuplicateChars getDuplicateChars = new GetDuplicateChars();
+		getDuplicateChars.getDuplicateCharacters("aakanksha");
+	}
+}
