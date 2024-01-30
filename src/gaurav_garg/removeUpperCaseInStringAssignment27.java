21afd3f9a4e618f4cb1c404a3b924da58774c3bd
Gaurav Garggit config --global user.name "Gaurav Garg <gauravgarg9136@gmail.com>
2024-01-30 10:24:32 +0530

    Gaurav_Assigment_27

---
+++ src/gaurav_garg/removeUpperCaseInStringAssignment27.java
@@ -0,0 +1,22 @@
+package gaurav_garg;
+
+public class removeUpperCaseInStringAssignment27 {
+
+	void removeUpperCaseInString(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			char char1 = str.charAt(i);
+			boolean value = Character.isUpperCase(char1);
+
+			if (value == true) {
+				str = str.replace(char1, '*');
+			}
+
+		}
+		str = str.replace("*", "");
+		System.out.print(str);
+	}
+
+	public static void main(String[] args) {
+		new removeUpperCaseInStringAssignment27().removeUpperCaseInString("TechNoCREdits");
+	}
+}
