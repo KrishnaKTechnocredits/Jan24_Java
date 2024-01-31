2f20464e6dfc8034ff1c0d9cbd733ee837cdb1e6
PriyaThakral <priyathakral90@gmail.com>
2024-01-31 18:09:18 +0530

    ProgrammingTest_1

---
+++ src/priya_t/Assignment29_DiffbetSumofEvenOddNo.java
@@ -0,0 +1,34 @@
+package priya_t;
+
+public class Assignment29_DiffbetSumofEvenOddNo {
+
+	boolean printDiff(String str) {
+		int sum = 0;
+		int sumEven = 0;
+		int sumOdd = 0;
+		String s = "te3ch4noc7re9d8its";
+
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if (Character.isDigit(ch)) {
+				int b = Integer.parseInt(String.valueOf(ch));
+				if (ch % 2 == 0)
+					sumEven = sumEven + b;
+				else {
+					sumOdd = sumOdd + b;
+				}
+			}
+		}
+		System.out.println(" Sum of Even No:" + sumEven);
+		System.out.println(" Sum of Odd No:" + sumOdd);
+
+		int diff = sumOdd - sumEven;
+		System.out.println(" Diff of sum of Even and odd digits:" + diff);
+		return false;
+	}
+
+	public static void main(String[] args) {
+		Assignment29_DiffbetSumofEvenOddNo diff = new Assignment29_DiffbetSumofEvenOddNo();
+		diff.printDiff("te3ch4noc7re9d8its");
+	}
+}
