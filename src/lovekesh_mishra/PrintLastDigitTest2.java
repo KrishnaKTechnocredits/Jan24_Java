571c78a006b830ffbdc339bfee24bf3a4c137b08
Lovekesh mishra <Lovekesh mishra>
2024-02-02 18:35:16 +0530

    LovekeshM_Test_2

---
+++ src/lovekesh_mishra/PrintLastDigitTest2.java
@@ -0,0 +1,24 @@
+class PrintLastDigitTest2{
+	char temp;
+
+	char Print(String input) {
+		for (int i = 0; i < input.length(); i++) {
+			char ch = input.charAt(i);
+			if (ch != 'a' || ch != 'e' || ch != 'i' || ch != 'o' || ch != 'u') {
+				if (input.indexOf(ch) == input.lastIndexOf(ch)) {
+					temp = ch;
+				}
+
+			}
+		}
+
+		return temp;
+
+	}
+
+	public static void main(String[] args) {
+		PrintLastDigitTest2 printLastDigit = new PrintLastDigitTest2();
+		char temp = printLastDigit.Print("aakanksha");
+		System.out.println(temp);
+	}
+}
