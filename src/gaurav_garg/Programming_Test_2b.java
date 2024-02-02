6fe65f70a744cbd068c4f255dd1cd28e74fba007
Gaurav Garggit config --global user.name "Gaurav Garg <gauravgarg9136@gmail.com>
2024-02-02 10:06:06 +0530

    Gaurav_Programing_test2b Update

--- src/gaurav_garg/Programming_Test_2b.java
+++ src/gaurav_garg/Programming_Test_2b.java
@@ -2,21 +2,30 @@ package gaurav_garg;
 
 public class Programming_Test_2b {
 
-	void printLastConstantValue(String str)
-	{
-		for(int i=str.length()-1;i>=0;i--)
-		{
-			char c=str.charAt(i);
-			if(c !='a' && c!='e' && c!='i'&& c!='o'&& c!='u' )
-			{
-				System.out.print(c);
-				break;
+	boolean nonRepeataiveCharcter(String str, char c) {
+		int i = str.indexOf(c);
+		int j = str.lastIndexOf(c);
+		if (i == j) {
+			return true;
+		}
+		return false;
+	}
+
+	void printLastConstantValueNonRepatative(String str) {
+		for (int i = str.length() - 1; i >= 0; i--) {
+			char c = str.charAt(i);
+			boolean answer = nonRepeataiveCharcter(str, c);
+			if (answer == true) {
+				if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+					System.out.print(c);
+					break;
+				}
 			}
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		new Programming_Test_2b().printLastConstantValue("aakansha");
+		new Programming_Test_2b().printLastConstantValueNonRepatative("aakanschhe");
 	}
 }
