9a58e758b0ee37656fdb6f685cc5dcea0f01450a
ankitasambhare12 <157062347+ankitasambhare12@users.noreply.github.com>
2024-02-03 21:29:10 +0530

    Update LastEevenNoInString.java

--- src/ankita_s/LastEevenNoInString.java
+++ src/ankita_s/LastEevenNoInString.java
@@ -2,24 +2,21 @@ package ankita_s;
 
 public class LastEevenNoInString {
 	int temp2;
-	void printLastEevenNoInString(String Str) 
-		{
-		for(int index=0;index<Str.length();index++)
-		{
+
+	void printLastEevenNoInString(String Str) {
+		for (int index = 0; index < Str.length(); index++) {
 			char ch = Str.charAt(index);
 			boolean temp = Character.isDigit(ch);
-			int temp1=Character.getNumericValue(ch);
-			if(temp==true && temp1%2==0)
-			{
-				temp2=temp1;
-				}
+			int temp1 = Character.getNumericValue(ch);
+			if (temp == true && temp1 % 2 == 0) {
+				temp2 = temp1;
 			}
-	   System.out.println("Last even no is:- " +temp2);
+		}
+		System.out.println("Last even no is:- " + temp2);
 	}
-	
-public static void main(String[]ars)
-	{
-		LastEevenNoInString lastEevenNoInString=new LastEevenNoInString();
+
+	public static void main(String[] ars) {
+		LastEevenNoInString lastEevenNoInString = new LastEevenNoInString();
 		lastEevenNoInString.printLastEevenNoInString("ankita2vitthal8sambhare9");
 	}
 }
