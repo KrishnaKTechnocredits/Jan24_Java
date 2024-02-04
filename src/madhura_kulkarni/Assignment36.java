751cc607b8bbb82ef625cd605043ac91b31b92f1
madhurakulkarni009 <madhura.kulkarni009@gmail.com>
2024-02-05 00:14:17 +0530

    src/madhura_kulkarni/Assignment36.java

--- src/madhura_kulkarni/Assignment36.java
+++ src/madhura_kulkarni/Assignment36.java
@@ -13,7 +13,7 @@ package madhura_kulkarni;
 
 public class Assignment36 {
 
-	void getSumOfEvenNumbers(int[] input) {
+	void getSumOfNumbers(int[] input) {
 int sum=0;
 for(int i=0;i<input.length;i++)
 {
@@ -26,7 +26,7 @@ System.out.println("Sum of numbers in string that are divisible by 3 or 5 is : "
 	public static void main(String[] args) {
 		int[] arr = {6, 5, 3, 15, 19, 40, 30, 27, 11};
 		Assignment36 ass = new Assignment36();
-		ass.getSumOfEvenNumbers(arr);
+		ass.getSumOfNumbers(arr);
 	}
 
 }
