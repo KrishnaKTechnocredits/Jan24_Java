9a4a4643de3c3001d9136d1a5e5a9ef63c5ab7df
Ashwini Balki <ashwinibodhe13@gmail.com>
2024-02-04 20:26:03 +0530

    Ashwini_Assignment_35

---
+++ src/ashwini_b/SumOfEvenIndex.java
@@ -0,0 +1,29 @@
+package ashwini_b;
+
+/*
+Assignment - 35 : 4th Feb'2024
+Return the sum of even index [0,2,4,6]
+input : {3, 6, 9, 11, 15, 20, 4, 26}
+Output : 60
+hint 6, 20, 4, 26
+ */
+public class SumOfEvenIndex {
+
+	int sumOfEvenIndex(int[] num) {
+		int sum = 0;
+		for (int index = 0; index < num.length; index++) {
+			if (index % 2 == 0 || index == 0) {
+				sum = sum + num[index];
+				// System.out.println(num[index]);
+			}
+		}
+		System.out.println(sum);
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int[] inputarry = { 3, 6, 9, 11, 15, 20, 4, 26 };
+		SumOfEvenIndex sumOfEvenIndex = new SumOfEvenIndex();
+		sumOfEvenIndex.sumOfEvenIndex(inputarry);
+	}
+}
