735baef39417c543e6cac925083d49218ed653bc
dharmakanani <dharmakanani792@gmail.com>
2024-02-05 00:35:45 +0530

    Sveta_Assignment_35

---
+++ src/sveta/Assignment_35_SumOfEvenNoIndex.java
@@ -0,0 +1,27 @@
+/*Assignment - 35 : 4th Feb'2024
+Return the sum of even numbers from the array
+input : {3, 6, 9, 11, 15, 20, 4, 26}
+Output : 60
+hint 6, 20, 4, 26*/
+
+package sveta;
+
+public class Assignment_35_SumOfEvenNoIndex {
+
+	public static void evenNoSum(int[] arr) {
+		int sum=0;
+		for(int index=0;index<arr.length;index++){
+			if(arr[index]%2==0) {
+				sum += arr[index];
+				System.out.println(arr[index]);
+			}
+		}
+		System.out.println(sum);
+	}
+	public static void main(String[] args) {
+		int[] arr={3, 6, 9, 11, 15, 20, 4, 26};
+		evenNoSum(arr);
+		
+	}
+
+}
