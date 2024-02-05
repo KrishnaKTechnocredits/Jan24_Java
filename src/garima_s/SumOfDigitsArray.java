a2e3dd9a3c7c0af6a43b3eac2f94996209c8b934
Garima <garimasinghal2285@gmail.com>
2024-02-05 15:22:38 +0530

    GarimaS_Assignment_44

---
+++ src/garima_s/SumOfDigitsArray.java
@@ -0,0 +1,28 @@
+package garima_s;
+
+public class SumOfDigitsArray {
+
+	boolean sumDigit(String name) {
+		int sum = 0;
+		for (int index = 0; index < name.length(); index++) {
+			char ch = name.charAt(index);
+			int digit = Character.getNumericValue(ch);
+			if (Character.isDigit(ch) == true) {
+				sum = sum + digit;
+			}
+		}
+		System.out.println(name + " -> " + sum);
+		return true;
+	}
+
+	void printNames(String[] names) {
+		for (int index = 0; index < names.length; index++) {
+			sumDigit(names[index]);
+		}
+	}
+
+	public static void main(String[] args) {
+		String[] names = { "Aakansha", "Aas3hvi", "Isha", "I2sh4a4n" };
+		new SumOfDigitsArray().printNames(names);
+	}
+}
