207b5dd22fb1ba7249e64d11b706502af0ca49d2
madhurakulkarni009 <madhura.kulkarni009@gmail.com>
2024-02-05 22:46:20 +0530

    src/madhura_kulkarni/Assignment46.java

--- src/madhura_kulkarni/Assignment46.java
+++ src/madhura_kulkarni/Assignment46.java
@@ -14,46 +14,25 @@ public class Assignment46 {
 
 	}
 
-	int getUpperCaseLettersCountInString(String input) {
-		int countUpper = 0;
-		for (int i = 0; i < input.length(); i++) {
-			char ch = input.charAt(i);
-			if (Character.isUpperCase(ch) == true) {
-				countUpper++;
-			}
-		}
-		return countUpper;
-	}
-
-	int getLowerCaseLettersCountInString(String input) {
-		int countLower = 0;
-		for (int i = 0; i < input.length(); i++) {
-			char ch = input.charAt(i);
-			if (Character.isLowerCase(ch) == true) {
-				countLower++;
-			}
-		}
-		return countLower;
-	}
-
 	void printString(String str) {
-		System.out.println("Input string is: " +str);
-		String upperCase= "";
-		String lowerCase= "";
-		String digit= "";
-		String answer="";
-		System.out.println("Rearranging a string in a way having all digits first followed by Uppercase followed by lowercase :");
+		System.out.println("Input string is: " + str);
+		String upperCase = "";
+		String lowerCase = "";
+		String digit = "";
+		String answer = "";
+		System.out.println(
+				"Rearranging a string in a way having all digits first followed by Uppercase followed by lowercase :");
 		for (int i = 0; i < str.length(); i++) {
 			char ch = str.charAt(i);
 			if (Character.isUpperCase(ch) == true)
-				upperCase= upperCase + ch;
+				upperCase = upperCase + ch;
 			else if (Character.isLowerCase(ch) == true)
-					lowerCase= lowerCase +ch;
+				lowerCase = lowerCase + ch;
 			else if (Character.isDigit(ch) == true)
-				digit= digit +ch; 
-						
+				digit = digit + ch;
+
 		}
-		answer= digit + upperCase + lowerCase;
+		answer = digit + upperCase + lowerCase;
 		System.out.println(answer);
 	}
 }
