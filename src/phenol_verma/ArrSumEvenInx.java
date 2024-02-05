96e58fc6fda9dee20cc9d6bdde0d2c5e56207021
PhenolVerma <phenol.verma@gmail.com>
2024-02-05 23:35:38 +0530

    PhenolV_Assignment_40

--- src/phenol_verma/ArrSumEvenInx.java
+++ src/phenol_verma/ArrSumEvenInx.java
@@ -1,3 +1,12 @@
+/*
+ * Assignment - 34 : 4th Feb'2024
+PhenolV_Assignment_40
+Return the sum of even index [0,2,4,6]
+input : {3, 6, 9, 11, 15, 20, 4, 26}
+Output : 60
+
+hint 6, 20, 4, 26
+ */
 package phenol_verma;
 
 public class ArrSumEvenInx {
