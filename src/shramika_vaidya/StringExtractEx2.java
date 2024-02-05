113171f90724d8d898974b68222a18dfc3578ae9
Shramika <shramikavaidya@gmail.com>
2024-02-05 19:13:14 +0530

    Shramika_Assignment47

---
+++ src/shramika_vaidya/StringExtractEx2.java
@@ -0,0 +1,19 @@
+package shramika_vaidya;
+
+public class StringExtractEx2 {
+
+	void getStringExtract(String str) {
+		String str3 = "";
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (Character.isUpperCase(ch) || Character.isLowerCase(ch)) {
+				str3 = str3 + ch;
+			}
+		}
+		System.out.println(str3);
+	}
+
+	public static void main(String[] args) {
+		new StringExtractEx2().getStringExtract("Aas-H3v7i");
+	}
+}
