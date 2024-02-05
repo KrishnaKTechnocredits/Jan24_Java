b8bb159bf8e9377ce1499f7816f79cefb7f3283e
Garima <garimasinghal2285@gmail.com>
2024-02-05 14:03:00 +0530

    GarimaS_Assignment_41

--- src/garima_s/StartWithVowelAndAtleast1Digit.java
+++ src/garima_s/StartWithVowelAndAtleast1Digit.java
@@ -11,12 +11,13 @@ public class StartWithVowelAndAtleast1Digit {
 	}
 
 	boolean isLeast1Digit(String name) {
-		for(int index=0;index<name.length();index++) {
-		char ch = name.charAt(index);
-		if(Character.isDigit(ch)==true) 
-			return true;
+		for (int index = 0; index < name.length(); index++) {
+			char ch = name.charAt(index);
+			if (Character.isDigit(ch) == true)
+				return true;
 
-	}return false;
+		}
+		return false;
 	}
 
 	void printNames(String[] names) {
@@ -26,9 +27,9 @@ public class StartWithVowelAndAtleast1Digit {
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		String[] names= {"Aakansha","Aas3hvi","Isha","I2sh4a4n","Var2sha"};
+		String[] names = { "Aakansha", "Aas3hvi", "Isha", "I2sh4a4n", "Var2sha" };
 		new StartWithVowelAndAtleast1Digit().printNames(names);
-}
+	}
 }
