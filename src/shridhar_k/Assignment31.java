bf8679c6db4f6d06f8399eabb6c72bf1d7df92ac
Shridhar Kumbhar <shridhartechno@gmail.com>
2024-02-05 09:37:24 +0530

    ShridharK_Assignment_31

---
+++ src/shridhar_k/Assignment31.java
@@ -0,0 +1,47 @@
+//Write a program to find character having maximum frequency from given String.
+
+package shridhar_k;
+
+public class Assignment31 {
+	int getFreqencyOfChar(String inputStr , char ch) {
+		int count = 0;
+		for (int i =0 ; i < inputStr.length() - 1; i++) {
+			if(inputStr.charAt(i) == ch) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	void processData(String inputStr) {
+		int maxFreq = 1;
+		char charOfMaxFreq = ' ';
+		System.out.println("input: " + inputStr);
+		for(int i =0; i < inputStr.length() - 1 ; i++) {
+			char ch = inputStr.charAt(i);
+			int charFreq = getFreqencyOfChar(inputStr, ch);
+			if (maxFreq < charFreq) {
+				maxFreq = charFreq;
+				charOfMaxFreq = ch;
+			}
+		}
+		if(maxFreq == 1) {
+			System.out.println("Input string '" + inputStr + "' has all unique character.");
+		}
+		else
+			System.out.println("Output: " + inputStr + " -> " + charOfMaxFreq + " -> " + maxFreq);
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Assignment31 assignment31 = new Assignment31();
+		String input = "aakanksha";
+		assignment31.processData(input);
+		input = "teecccchnoceredites";
+		assignment31.processData(input);
+		input = "maulik";
+		assignment31.processData(input);
+
+	}
+
+}
