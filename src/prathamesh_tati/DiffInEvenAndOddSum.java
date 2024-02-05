cef9292d9de9eb5eab0ebf6e6e81be540bb6b3d7
Prathamesh Tati <tati.pratham@gmail.com>
2024-02-05 08:15:40 +0530

    PrathameshT_Assignment_34

---
+++ src/prathamesh_tati/DiffInEvenAndOddSum.java
@@ -0,0 +1,29 @@
+package prathamesh_tati;
+
+public class DiffInEvenAndOddSum {
+	static int evenSum = 0;
+	static int oddSum = 0;
+
+	static void getEvenOddSum(int[] arr) {
+		for (int index = 0; index < arr.length; index++) {
+			if (arr[index] % 2 == 0)
+				evenSum = evenSum + arr[index];
+			else
+				oddSum = oddSum + arr[index];
+		}
+	}
+
+	void displaySum(int[] input) {
+		getEvenOddSum(input);
+		if (evenSum >= oddSum)
+			System.out.println("Difference between Even sum and Odd Sum is = " + (evenSum - oddSum));
+		else
+			System.out.println("Difference between Even sum and Odd Sum is = " + (oddSum - evenSum));
+	}
+
+	public static void main(String[] args) {
+		DiffInEvenAndOddSum diffInEvenAndOddSum = new DiffInEvenAndOddSum();
+		int[] num = { 30, 24, 19, 13, 36, 51, 11, 72 };
+		diffInEvenAndOddSum.displaySum(num);
+	}
+}
