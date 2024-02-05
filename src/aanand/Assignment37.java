2be8fceeb6a410cbaf16c7b330390e217f4a075d
Anand Rathod <rathod.a86@gmail.com>
2024-02-06 00:14:12 +0530

    AnandR_Assignment_39

---
+++ src/aanand/Assignment37.java
@@ -0,0 +1,13 @@
+package aanand;
+
+public class Assignment37 {
+	int WordFreqInArray(String name) {
+		int count = 0;
+		for(int i = 0; i < name.length(); i++){
+			char ch = name.charAt(i);
+			
+		}
+		
+		return count;
+	}
+}
