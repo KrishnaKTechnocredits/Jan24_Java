5521918c38ec810d8a3ca045aa333810f0a33617
Varsha Nagadwala <varshakhatri38@gmail.com>
2024-02-06 20:03:37 +0530

    Varshan_Assignment_47

---
+++ src/varsha/ArrayEx16.java
@@ -0,0 +1,19 @@
+package varsha;
+
+public class ArrayEx16 {
+
+	void printString(String input) {
+		String str = "";
+		for (int i = 0; i < input.length(); i++) {
+			char ch = input.charAt(i);
+			if (!Character.isDigit(ch)) {
+				str = str + ch;
+			}
+		}
+		System.out.println(str);
+	}
+
+	public static void main(String[] args) {
+		new ArrayEx16().printString("Aas-H3v7i");
+	}
+}
