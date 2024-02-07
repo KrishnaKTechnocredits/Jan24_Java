6e89eae891c14a4eccb87ebe6fe98c5c75273f7e
Ankita Sambhare <ankitasambhare94@gmail>
2024-02-07 11:33:42 +0530

    AnkitaS_Assignment_45

---
+++ src/ankita_s/Assignment45Array.java
@@ -0,0 +1,41 @@
+package ankita_s;
+
+public class Assignment45Array {
+	void countofupperandlower(String[] str) {
+		for (int index = 0; index < str.length; index++) {
+			int CountUpperCase = isupper(str[index]);
+			int CountLowerCase = islower(str[index]);
+			if (CountUpperCase > CountLowerCase) {
+				System.out.println(str[index]);
+			}
+		}
+	}
+
+	int isupper(String str) {
+		int count = 0;
+		for (int index = 0; index < str.length(); index++) {
+			char ch = str.charAt(index);
+			boolean temp = Character.isUpperCase(ch);
+			if (temp == true)
+				count++;
+		}
+		return count;
+	}
+
+	int islower(String str) {
+		int count = 0;
+		for (int index = 0; index < str.length(); index++) {
+			char ch = str.charAt(index);
+			boolean temp = Character.isLowerCase(ch);
+			if (temp == true)
+				count++;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		String[] arr = { "aAShVI", "TEchNoCRediTs", "ANSh", "dhARMma", "vIDhItA" };
+		Assignment45Array ass45 = new Assignment45Array();
+		ass45.countofupperandlower(arr);
+	}
+}
