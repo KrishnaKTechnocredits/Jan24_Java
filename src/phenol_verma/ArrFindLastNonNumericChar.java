e78d32bb114955a1210d43f0be106e2e84af2243
PhenolVerma <phenol.verma@gmail.com>
2024-02-08 13:22:39 +0530

    PhenolV_Assignment_54

--- src/phenol_verma/ArrFindLastNonNumericChar.java
+++ src/phenol_verma/ArrFindLastNonNumericChar.java
@@ -1,24 +1,53 @@
+/*
+ Assignment - 49 : 8th Feb'2024
+ PR - PhenolV_Assignment_54
+ Condition 1 - If last digit numeric, print second last character
+ Condition 2 - If last digit non numeric, print last character
+Input = String arr[] = { "Phenol17", "Neha2", "Lalit", "Maulik123", "Harsh1", "Ma1r3g4i5" };
+Output - 
+1
+a
+t
+2
+h
+i
+  */
+
 package phenol_verma;
 
 public class ArrFindLastNonNumericChar {
+	private String arr[];
 
-	void nonNumricChar(String arr[]) {
+	char[] nonNumricChar(String arr[]) {
+
+		char finalChar[] = new char[6];
+
 		for (int index = 0; index < arr.length; index++) {
 			String str = arr[index];
-			//for (int charIndex = 0; charIndex < str.length(); charIndex++) {
-				char ch = str.charAt(str.length()-1);
-				if (Character.isDigit(ch))
-					System.out.println(str.charAt(str.length()-2));
-				else
-					System.out.println(str.charAt(str.length()-1));
-			}
+			this.arr = arr;
+			if (Character.isDigit(str.charAt(str.length() - 1)))
+				// If last digit numeric, capture second last character
+				finalChar[index] = str.charAt(str.length() - 2);
+			else
+				// If last digit non numeric, capture last character
+				finalChar[index] = str.charAt(str.length() - 1);
 		}
+		return finalChar;
+	}
 
-	
+	void printFinalArr(char finArr[]) {
+		for (int finalIndex = 0; finalIndex < finArr.length; finalIndex++) {
+			System.out.println("Second last charater of string " + arr[finalIndex] + " -> " + finArr[finalIndex]);
+		}
+	}
 
 	public static void main(String[] args) {
 		ArrFindLastNonNumericChar arrFindLastNonNumericChar = new ArrFindLastNonNumericChar();
 		String arr[] = { "Phenol17", "Neha2", "Lalit", "Maulik123", "Harsh1", "Ma1r3g4i5" };
-		arrFindLastNonNumericChar.nonNumricChar(arr);
+
+		char[] finalChar = arrFindLastNonNumericChar.nonNumricChar(arr);
+
+		arrFindLastNonNumericChar.printFinalArr(finalChar);
+
 	}
 }
