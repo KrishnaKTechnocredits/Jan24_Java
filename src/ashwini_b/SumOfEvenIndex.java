3229194eaf30e1e352283876f09ea938247591a5
Ashwini Balki <ashwinibodhe13@gmail.com>
2024-02-09 22:36:50 +0530

    Removed unnecessary condition and corrected evidence output in comments

--- src/ashwini_b/SumOfEvenIndex.java
+++ src/ashwini_b/SumOfEvenIndex.java
@@ -4,15 +4,15 @@ package ashwini_b;
 Assignment - 35 : 4th Feb'2024
 Return the sum of even index [0,2,4,6]
 input : {3, 6, 9, 11, 15, 20, 4, 26}
-Output : 60
-hint 6, 20, 4, 26
+Output : 31
+hint 3, 9, 15, 4
  */
 public class SumOfEvenIndex {
 
 	int sumOfEvenIndex(int[] num) {
 		int sum = 0;
 		for (int index = 0; index < num.length; index++) {
-			if (index % 2 == 0 || index == 0) {
+			if (index % 2 == 0) {
 				sum = sum + num[index];
 			}
 		}
