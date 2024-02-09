833dbe01c24b5f2688aeb09650a462f5b7b2756c
PhenolVerma <phenol.verma@gmail.com>
2024-02-09 14:27:24 +0530

    PhenolV_Assignment_56

--- src/phenol_verma/StringCountDigitInString.java
+++ src/phenol_verma/StringCountDigitInString.java
@@ -1,5 +1,6 @@
 /*
 Assignment - 50 : 9th Feb'2024
+PhenolV_Assignment_56
 WAP to find count of digits in a given String.
 String str = "H2i H3el4lo P4un2e5";
 output : 6
@@ -10,13 +11,17 @@ package phenol_verma;
 public class StringCountDigitInString {
 
 	void stringDigitCount(String str) {
-		String finalStr = "";
+		int countDigit = 0;
+		int countDigitSum = 0;
 		for (int index = 0; index < str.length(); index++) {
-			if (!Character.isDigit(str.charAt(index))) {
-				finalStr = finalStr + str.charAt(index);
+			if (Character.isDigit(str.charAt(index))) {
+				char ch = str.charAt(index);
+				countDigit++;
+				countDigitSum = countDigitSum + Character.getNumericValue(ch);
 			}
 		}
-		System.out.println("String after digits removal - " + finalStr);
+		System.out.println("Digit sum from string - " + countDigitSum);
+		System.out.println("Digit count from string - " + countDigit);
 	}
 
 	public static void main(String[] args) {
