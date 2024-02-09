48f27580ff9854516fd9a5eba3d675f7f0edbf55
Rupali Umagol <rupali.umagol@gmail.com>
2024-02-09 10:50:33 +0530

    RupaliU_Assignment_50

---
+++ src/rupali_umagol/Assignment50.java
@@ -0,0 +1,19 @@
+package rupali_umagol;
+
+public class Assignment50 {
+	int getDigitFromString(String str) {
+		int count=0;
+		for(int i=0; i<str.length();i++) {
+			char ch=str.charAt(i);
+			if(Character.isDigit(ch))
+				count++;
+		}
+		return count;
+	}
+	public static void main(String[] args) {
+		Assignment50 assignment50=new Assignment50();
+		String str="H2i H3el4lo P4un2e5";
+		int result=assignment50.getDigitFromString(str);
+		System.out.println(result);
+	}
+}
