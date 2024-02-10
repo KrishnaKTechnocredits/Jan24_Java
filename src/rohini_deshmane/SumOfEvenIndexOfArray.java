fefbf1e2780bb4a1c7e953f23688210a786c6e51
Rohini Deshmane <Rohindeshmane.21@gmail.com>
2024-02-10 20:40:55 +0530

    RohiniD_Assignment_35 Update

--- src/rohini_deshmane/SumOfEvenIndexOfArray.java
+++ src/rohini_deshmane/SumOfEvenIndexOfArray.java
@@ -2,19 +2,20 @@ package rohini_deshmane;
 /*
  * Assignment - 35 : 4th Feb'2024
 
-Return the sum of even index number from the array
+Return the sum of even index from the array
 
 input : {3, 6, 9, 11, 15, 20, 4, 26}
-Output : 56
+Output : 31
 
-hint 6, 20, 4, 26
+hint: 3,9,15,4
  */
 public class SumOfEvenIndexOfArray {
 	
 	void printSum(int[] arr) {
 		int sum=0;
 		for(int i=0; i< arr.length; i++) {
-			if(arr[i]%2 ==0)
+
+			if(i%2==0)
 				sum=sum+arr[i];
 		}
 		System.out.println("Sum of event index numbers = "+sum);
@@ -24,6 +25,6 @@ public class SumOfEvenIndexOfArray {
 		SumOfEvenIndexOfArray sumOfNum = new SumOfEvenIndexOfArray();
 		int[] arr = {3, 6, 9, 11, 15, 20, 4, 26};
 		sumOfNum.printSum(arr);
-		
+
 	}
 }
