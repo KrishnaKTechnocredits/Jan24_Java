fc1c56e9d254b1c738276a2de557cf6618bda9aa
Rohini Deshmane <Rohindeshmane.21@gmail.com>
2024-02-10 18:41:22 +0530

    RohiniD_Assignment_35 Update

--- src/rohini_deshmane/SumOfEvenIndexOfArray.java
+++ src/rohini_deshmane/SumOfEvenIndexOfArray.java
@@ -14,7 +14,7 @@ public class SumOfEvenIndexOfArray {
 	void printSum(int[] arr) {
 		int sum=0;
 		for(int i=0; i< arr.length; i++) {
-			if(i%2==0)
+			if(arr[i]%2==0)
 				sum=sum+arr[i];
 		}
 		System.out.println("Sum of event index numbers = "+sum);
