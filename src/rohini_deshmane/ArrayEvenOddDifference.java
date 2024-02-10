5814a1e4c61e04007b9c78552ac1f042cd67747d
Rohini Deshmane <Rohindeshmane.21@gmail.com>
2024-02-10 20:15:18 +0530

    RohiniD_Assignment_34 Update

--- src/rohini_deshmane/ArrayEvenOddDifference.java
+++ src/rohini_deshmane/ArrayEvenOddDifference.java
@@ -1,4 +1,13 @@
 package rohini_deshmane;
+/*
+ * Return the positive difference between even number sum and odd number sum.
+
+input : {30, 24, 19, 13, 36, 51, 11, 72}
+Output : 162 - 94 = 68
+
+evenNum= 30+24+36+72 =162
+Odd= 19+13+51+11 = 94 
+ */
 
 public class ArrayEvenOddDifference {
 	static int eventSum, oddSum;
