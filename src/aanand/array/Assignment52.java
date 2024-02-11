35bc6c5304e11fabd6108d7a69233bff312d990d
Anand Rathod <rathod.a86@gmail.com>
2024-02-12 01:01:24 +0530

    AnandR_Assignment_52

---
+++ src/aanand/array/Assignment52.java
@@ -0,0 +1,29 @@
+package aanand.array;
+
+public class Assignment52 {
+
+	int getDigitFromString(String str) {
+		String ans = "0";
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (Character.isDigit(ch)) {
+				ans = ans + ch;
+			}
+		}
+		return Integer.parseInt(ans);
+	}
+
+	void getNumFromArray(String[] arr) {
+		int[] num = new int[arr.length];
+		for (int index = 0; index < arr.length; index++) {
+			num[index] = getDigitFromString(arr[index]);
+			System.out.println(num[index]);
+		}
+	}
+
+	public static void main(String[] args) {
+		String[] input = { "tech1no3cr4edits", "prathm5es6h", "a4ksha7y", "Padmaja" };
+		Assignment52 assignment52 = new Assignment52();
+		assignment52.getNumFromArray(input);
+	}
+}
