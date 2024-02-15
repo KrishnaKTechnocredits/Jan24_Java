bb9bd55dd81b0bbd53da4f3fb612622ecade2b82
Garima <garimasinghal2285@gmail.com>
2024-02-15 11:26:31 +0530

    GarimaS_Assignment_60

--- src/garima_s/PrintSecondMaxFrequency.java
+++ src/garima_s/PrintSecondMaxFrequency.java
@@ -6,8 +6,6 @@ output: k->2"
 package garima_s;
 
 public class PrintSecondMaxFrequency {
-	int max, secMax;
-	char chMax;
 
 	int getCountCharacter(String input, char targetChar) {
 		int count = 0;
@@ -19,19 +17,26 @@ public class PrintSecondMaxFrequency {
 		return count;
 	}
 
-	void printSecondMaxCharacter(String input) {
+	char getMaxCharacter(String input) {
+		int max = 0;
+		char chMax = ' ';
 		for (int index = 0; index < input.length(); index++) {
 			char ch = input.charAt(index);
 			int count = getCountCharacter(input, ch);
 			if (max < count) {
 				max = count;
 				chMax = ch;
-			} else if (secMax < count && max > count) {
-				secMax = count;
-				chMax = ch;
 			}
 		}
-		System.out.println(chMax + "-->" + secMax);
+		return chMax;
+	}
+
+	void printSecondMaxCharacter(String input) {
+		char ch = getMaxCharacter(input);
+		String str = input.replace(String.valueOf(ch), "");
+		ch = getMaxCharacter(str);
+		int count = getCountCharacter(str, ch);
+		System.out.println(ch + "-->" + count);
 	}
 
 	public static void main(String[] args) {
