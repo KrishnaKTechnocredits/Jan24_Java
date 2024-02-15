7cc4ec8e86293879b8eb54cdb13602742148184d
Shridhar Kumbhar <shridhartechno@gmail.com>
2024-02-16 00:47:54 +0530

    updated logic : made coustomised

--- src/shridhar_k/Assignment60.java
+++ src/shridhar_k/Assignment60.java
@@ -10,7 +10,10 @@ public class Assignment60 {
 
 	int getFrequencuOfCharacter(String inpStr, char inpCh) {
 		int count = 0;
-		if (inpStr.indexOf(inpCh) == inpStr.lastIndexOf(inpCh)) {
+		if(inpCh == '@') {
+			return -1;
+		}
+		else if (inpStr.indexOf(inpCh) == inpStr.lastIndexOf(inpCh)) {
 			return 1;
 		} else {
 			for (int i = 0; i < inpStr.length() ; i++) {
@@ -31,26 +34,32 @@ public class Assignment60 {
 				maxFreq = charFreq;
 				maxFreqCh = inpStr.charAt(i);
 			}
+			inpStr = inpStr.replace(inpStr.charAt(i), '@');
 		}
 		return maxFreqCh;
 	}
 
-	void printSecondHeighestFreqChar(String inpStr, int freqIndex) {
+	void printHeighestFreqCharOfGivenIndex(String originalString, int freqIndex) {
+		String inpStr = originalString;
 		char character = ' ';
 		if (freqIndex == 1) {
 			character = getMaxCharFreq(inpStr);
 		} else {
+			for(int i =1; i<= freqIndex; i++) {
 			character = getMaxCharFreq(inpStr);
 			inpStr = inpStr.replace(String.valueOf(character), "");
-			character = getMaxCharFreq(inpStr);
+			if(i != freqIndex) {
+				character = getMaxCharFreq(inpStr);
+			}
 		}
-		int characterFreq = getFrequencuOfCharacter(inpStr, character);
+	}
+		int characterFreq = getFrequencuOfCharacter(originalString, character);
 
 		System.out.println("output : " + character + " -> " + characterFreq);
 	}
 
 	public static void main(String[] args) {
 		String input = "aakanksha";
-		new Assignment60().printSecondHeighestFreqChar(input, 2);
+		new Assignment60().printHeighestFreqCharOfGivenIndex(input, 2);
 	}
 }
