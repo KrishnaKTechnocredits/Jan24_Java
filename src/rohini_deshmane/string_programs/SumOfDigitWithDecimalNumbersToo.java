009ef58d5aab84dc7d00f6759b043529f536ea6f
Rohini Deshmane <Rohindeshmane.21@gmail.com>
2024-02-16 20:10:45 +0530

    RohiniD_Assignment_62

--- src/rohini_deshmane/string_programs/SumOfDigitWithDecimalNumbersToo.java
+++ src/rohini_deshmane/string_programs/SumOfDigitWithDecimalNumbersToo.java
@@ -20,27 +20,41 @@ public class SumOfDigitWithDecimalNumbersToo {
 		return digitStr;
 	}
 
+	String getLongDigit(String str, int index) {
+		String longDigit ="";
+		for(int i=index; i<str.length(); i++) {
+			if(Character.isDigit(str.charAt(i))) {
+				longDigit = longDigit + str.charAt(i);
+			} else if(!Character.isDigit(str.charAt(i)))
+					break;
+		}
+		return longDigit;
+	}
+	
 	double findSumOfNumbers(String str) {
 		double sum=0;
 		String digit="";
-		for(int i=0; i<str.length();) {
+		for(int i=0; i<str.length(); i++) {
 			if(Character.isDigit(str.charAt(i))) {
+				digit = Character.toString(str.charAt(i));
 				if(str.charAt(i+1) =='.') {
-					digit = str.charAt(i)+"."+getDecimalDigitFromString(str, i+2);
+					String returndigit =getDecimalDigitFromString(str, i+2);
+					digit = digit+"."+returndigit;
+					i=i+digit.length()+1;
+				} else if(Character.isDigit(str.charAt(i+1))) {
+					String longDigit = getLongDigit(str, i);
+					digit = longDigit;
 				}
-				int cnt = digit.length();
-				i=i+cnt;
-				//sum = sum + Double.parseDouble(Character.toString(str.charAt(i)));
+				System.out.println(digit);
+				sum = sum+Double.parseDouble(digit);
 			}
-			else
-				i++;
 		}
 		return sum;
 	}
 	
 	public static void main(String[] args) {
 		SumOfDigitWithDecimalNumbersToo sumOfNegativeNumber = new SumOfDigitWithDecimalNumbersToo();
-		double sum = sumOfNegativeNumber.findSumOfNumbers("te3ch4.5noc7red8.1its");
+		double sum = sumOfNegativeNumber.findSumOfNumbers("te453ch4.589noc7red8.91its");
 		System.out.println("Sum Of number = "+sum);
 		System.out.println("\n");
 		sum = sumOfNegativeNumber.findSumOfNumbers("te3.67ch4noc7r4.7ed0.8its");
