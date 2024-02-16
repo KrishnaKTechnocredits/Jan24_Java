22b0f64226f4ecb2adb1e13c66521dbe702496e4
Hiral Purohit <rajpurohithiral@gmail.com>
2024-02-17 00:00:43 +0530

    Hiral_Assignment_63

---
+++ src/hiral_purohit/Assignment_63.java
@@ -0,0 +1,15 @@
+package hiral_purohit;
+
+public class Assignment_63 {
+	
+	void printEmptyString(String str) {
+		if(str.isEmpty()) {
+			System.out.println("Empty string, no character found.");
+		}
+		
+	}
+	public static void main(String[] args) {
+		new Assignment_63().printEmptyString("");
+	}
+
+}
