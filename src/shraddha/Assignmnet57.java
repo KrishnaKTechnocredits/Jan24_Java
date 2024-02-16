d368a2e6f5b5247d284917bbcc2fb88d582c7292
Shraddha More <shradddhamore@gmail.com>
2024-02-16 23:55:28 +0530

    Assignmnet_57

---
+++ src/shraddha/Assignmnet57.java
@@ -0,0 +1,30 @@
+package shraddha;
+
+class Assignmnet57{
+
+	boolean isDigit(char ch){
+		if(Character.isDigit(ch)){
+			return true;
+		}
+		return false;
+	}
+	void squareOf(String Str){
+		int sum =0;
+		for(int i=0;i<Str.length();i++){
+			char ch = Str.charAt(i);
+			if (isDigit(ch)){
+				int num = Character.getNumericValue(ch);
+				if(num%2!=0){
+					sum = sum + (num * num);
+				}
+			}
+		}
+			System.out.println(sum);
+	}
+		
+		public static void main(String[] args){
+			Assignmnet57 assignmnet = new Assignmnet57();
+			assignmnet.squareOf("te3ch4noc7red8its");
+		}
+		
+}
