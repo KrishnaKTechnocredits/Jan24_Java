47683a9e4eed6d466b1f3c64dd776d0760516b1e
Samiksha gadakh <samikshagadakh24@gmail.com>
2024-02-16 23:41:06 +0530

    Samiksha_Assignment_58

---
+++ src/samiksha/MinFreq.java
@@ -0,0 +1,36 @@
+package samiksha;
+
+public class MinFreq {
+
+	int getFreq(String str, char targetCh) {
+		int count = 0;
+		for (int index = 0; index < str.length(); index++) {
+			char ch = str.charAt(index);
+			if (ch == targetCh) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	void printLastFrquency(String str) {
+		int minCount = 100;
+		char minChar = ' ';
+		for (int index = 0; index < str.length(); index++) {
+			char ch = str.charAt(index);
+			int count = getFreq(str, ch);
+			if (minCount >= count) {
+				minCount = count;
+				if (minCount == 1) {
+					minChar = ch;
+				}
+			}
+		}
+		System.out.println(minChar);
+	}
+
+	public static void main(String[] args) {
+		String str = "aakanksha";
+		new MinFreq().printLastFrquency(str);
+	}
+}
