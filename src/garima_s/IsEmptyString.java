a05d4a44271b1a756234e867de4d10be495262c9
Garima <garimasinghal2285@gmail.com>
2024-02-16 13:17:52 +0530

    GarimaS_Assignment_63

---
+++ src/garima_s/IsEmptyString.java
@@ -0,0 +1,15 @@
+/*"Detect empty input string:
+input: """"
+output: Empty string, no character found."
+ */
+package garima_s;
+
+class IsEmptyString {
+
+	public static void main(String[] args) {
+		String input = "";
+		if (input.isEmpty()) {
+			System.out.println("Empty String,no Character Found");
+		}
+	}
+}
