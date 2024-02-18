e378df52509930233c645b0007f4799b81cda3f8
Varsha Nagadwala <varshakhatri38@gmail.com>
2024-02-18 20:03:54 +0530

    change the package

---
+++ src/varsha/inheritance_cases/Employee.java
@@ -0,0 +1,15 @@
+package varsha.inheritance_cases;
+
+public class Employee extends Manager {
+	int y = 100; 
+	int z = 200; 
+	
+	void m2() {
+		System.out.println("Good");
+	}
+	
+	void m3() {
+		System.out.println("Day");
+	}
+}
+ 
