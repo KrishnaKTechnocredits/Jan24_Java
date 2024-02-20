429c05341b5ffe9f0855ce3a9ee576fb3dfbdbf2
Hiral Purohit <rajpurohithiral@gmail.com>
2024-02-20 17:13:42 +0530

    Hiral_Assignment_68

---
+++ src/hiral_purohit/Manager.java
@@ -0,0 +1,15 @@
+package hiral_purohit;
+
+public class Manager {
+
+	int x = 10;
+	int y = 20;
+
+	void m1() {
+		System.out.println("technocredits");
+	}
+
+	void m2() {
+		System.out.println("Manager");
+	}
+}
