639fac2d42a37542e70fd379de23a5cac4877d81
dharmakanani <Svetakanani123@gmail.com>
2024-02-22 19:02:05 +0530

    Dharma_Assignment_67

---
+++ src/dharma/Assignment67.java
@@ -0,0 +1,61 @@
+package dharma;
+
+public class Assignment67 {
+
+	String splitString(String str) {
+		String str1 = "";
+		String reverseString = "";
+		for (int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == ' ') {
+				reverseString = reverseString + reverse(str1);
+				reverseString = reverseString + " ";
+				str1 = "";
+			}else {
+				str1 = str1 + str.charAt(i);
+			}
+		}
+		reverseString = reverseString + reverse(str1);
+		return reverseString;
+	}
+	
+	String reverse(String str) {
+		String str2 = "";
+		for(int i = str.length()-1; i >= 0; i--) {
+			str2 = str2 + str.charAt(i);
+		}
+		return str2;
+	}
+	
+	String initialCapital(String str) {
+		String str1 = "";
+		for(int i = 0; i < str.length(); i++) {
+			if(i == 0)
+				str1 = "" + Character.toUpperCase(str.charAt(i));
+			else if(str.charAt(i) == ' ') {
+				str1 = str1 + " ";
+				str1 = str1 + Character.toUpperCase(str.charAt(i+1));
+				i++;
+			}
+			else {
+				str1 = str1 + Character.toLowerCase(str.charAt(i));
+			}
+		}
+		return str1 + " ";
+	}
+	
+	String initialCapitalReverse(String str) {
+		String str1 = splitString(str); 
+		String str2 = initialCapital(str1);
+		return str2;
+	}
+	
+	public static void main(String[] args) {
+		Assignment67 assignment67 = new Assignment67();
+		String str = assignment67.splitString("Hi Hello Maulik");
+		System.out.println(str);
+		String str2 = assignment67.initialCapital("hi hEllO how are you");
+		System.out.println(str2);
+		String str3 = assignment67.initialCapitalReverse("Hi Hello Maulik");
+		System.out.println(str3);
+	}
+}
