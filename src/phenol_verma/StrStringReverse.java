253be993c58e74f4dfdd014e387d92941ff17d09
PhenolVerma <phenol.verma@gmail.com>
2024-02-22 14:29:44 +0530

    PhenolV_Assignment_72

--- src/phenol_verma/StrStringReverse.java
+++ src/phenol_verma/StrStringReverse.java
@@ -19,38 +19,74 @@ package phenol_verma;
 
 import java.util.Arrays;
 
-import sasmita.ReverseofString;
-
 public class StrStringReverse {
 
-	// input : Hi Hello Maulik, output : iH olleH kiluaM
-	void strReversePatr1(String str) {
-		String strArr[] = str.split(" ");
-		for (int index = 0; index < strArr.length; index++) {
-			//strArr[index] = reverseString(strArr[index]);
-			System.out.println(strArr[index]);
-
+	// Program 1
+	void prog1StrRev(String str) {
+		String[] splitStr = str.split(" ");
+		for (int index = 0; index < splitStr.length; index++) {
+			for (int inIndex = splitStr[index].length() - 1; inIndex >= 0; inIndex--) {
+				char chr1 = splitStr[index].charAt(inIndex);
+				System.out.print(chr1);
+			}
+			System.out.print(" ");
 		}
-
-		//System.out.println(Arrays.toString(strArr));
 	}
 
-	// String str = "hi hEllO how are you";, output : Hi Hello How Are You
-	void strReversePatr2(String str) {
-
-		System.out.println(str);
+	// Program 2
+	void prog2StrCaps(String str) {
+		String lowCaseString = str.toLowerCase();
+		for (int index = 0; index < lowCaseString.length(); index++) {
+			char strChar = lowCaseString.charAt(index);
+			if (index == 0) {
+				System.out.print(Character.toUpperCase(strChar));
+			} else if (strChar == ' ') {
+				System.out.print(" " + Character.toUpperCase(lowCaseString.charAt(index + 1)));
+				index++;
+			} else
+				System.out.print(strChar);
+		}
 	}
 
-	// input : Hi Hello Maulik, output : Ih Olleh Kiluam
-	void strReversePatr3(String str) {
-
-		System.out.println(str);
+	// Program 3
+	void prog3StrRevCaps(String str) {
+		String uperCaseString = str.toUpperCase();
+		String[] splitStr = uperCaseString.split(" ");
+		int lenght = 0;
+		for (int index = 0; index < splitStr.length; index++) {
+			for (int inIndex = splitStr[index].length() - 1; inIndex >= 0; inIndex--) {
+				lenght = splitStr[index].length();
+				char chr1 = splitStr[index].charAt(inIndex);
+				if (inIndex == lenght - 1) {
+					System.out.print(chr1);
+				} else {
+					System.out.print(Character.toLowerCase(chr1));
+				}
+			}
+			System.out.print(" ");
+		}
 	}
 
 	public static void main(String[] args) {
-		StrStringReverse strStringReverse = new StrStringReverse();
-		strStringReverse.strReversePatr1("Hi Hello Maulik");
-		strStringReverse.strReversePatr2("hi hEllO how are you");
-		strStringReverse.strReversePatr3("Hi Hello Maulik");
+		StrStringReverse strStringReverse1 = new StrStringReverse();
+
+		String str1 = "Hi Hello Maulik";
+		String str2 = "hi hEllO how are you";
+		String str3 = "Hi Hello Maulik";
+
+		System.out.println("Program 1 : Original String - " + str1);
+		System.out.println("Output should be - iH olleH kiluaM");
+		System.out.print("Program 1 : Output String - ");
+		strStringReverse1.prog1StrRev(str1);
+
+		System.out.println("\n\nProgram 2 : Original String - " + str2);
+		System.out.println("Output should be - Hi Hello How Are You");
+		System.out.print("Program 2 : Output String - ");
+		strStringReverse1.prog2StrCaps(str2);
+
+		System.out.println("\n\nProgram 3 : Original String - " + str3);
+		System.out.println("Output should be - Ih Olleh Kiluam");
+		System.out.print("Program 3 : Output String - ");
+		strStringReverse1.prog3StrRevCaps(str3);
 	}
 }
