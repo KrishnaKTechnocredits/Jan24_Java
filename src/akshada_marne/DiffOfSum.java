220048915e7fac06c4aa0c370614b5110c3cbef1
Akshada Rajput <akshada.marne@gmail.com>
2024-02-27 14:42:27 +0530

    AkshadaR_Assignment34

--- src/akshada_marne/DiffOfSum.java
+++ src/akshada_marne/DiffOfSum.java
@@ -3,33 +3,30 @@ package akshada_marne;
 public class DiffOfSum {
 
 	void sum(int[] num) {
-		int sumeven=0;
-		int sumodd=0;
-		int difference=0;
-		for(int i=0; i<num.length;i++) {
-			if(num[i]%2==0) {
-				sumeven=sumeven+num[i];	
-				}
-			if (num[i]%2!=0) {
-				sumodd=sumodd+num[i];	
-				}
-				
+		int sumeven = 0;
+		int sumodd = 0;
+		int difference = 0;
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] % 2 == 0) {
+				sumeven = sumeven + num[i];
 			}
-			if(sumeven>sumodd) {
-				difference=sumeven-sumodd;
+			if (num[i] % 2 != 0) {
+				sumodd = sumodd + num[i];
 			}
-			else {
-				difference=sumodd-sumeven;
-			}
-			
-			System.out.println("Difference of "+ sumeven + " and "+ sumodd +" is "+ difference	);
+
 		}
-		
-	
-	
+		if (sumeven > sumodd) {
+			difference = sumeven - sumodd;
+		} else {
+			difference = sumodd - sumeven;
+		}
+
+		System.out.println("Difference of " + sumeven + " and " + sumodd + " is " + difference);
+	}
+
 	public static void main(String args[]) {
-		DiffOfSum diffOfSum= new DiffOfSum();
-		int[] arr= {30, 24, 19, 13, 36, 51, 11, 72};
+		DiffOfSum diffOfSum = new DiffOfSum();
+		int[] arr = { 30, 24, 19, 13, 36, 51, 11, 72 };
 		diffOfSum.sum(arr);
 	}
 }
