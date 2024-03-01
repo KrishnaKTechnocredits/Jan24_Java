4b88dfb7e07bef3896eab5ef79ad7dae64ea9f31
sureshbabumc <sureshbabumc.@brillio.com>
2024-03-01 16:41:35 +0530

    suresh_Assign_60

---
+++ src/suresh/array/Assignment60.java
@@ -0,0 +1,55 @@
+/*
+ * "Print character with second-highest frequency:
+input: aakanksha
+output: k->2"
+ */
+package suresh.array;
+
+public class Assignment60 {
+
+	int getCharFrequecy(String input, char ch) {
+		int count = 0;
+		for (int i = 0; i < input.length(); i++) {
+			char ch1 = input.charAt(i);
+			if (ch1 == ch) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	char getMinFrequecy(String input) {
+		int maxcount = 0;
+		char maxchar = ' ';
+
+		for (int i = 0; i < input.length(); i++) {
+			char ch = input.charAt(i);
+			int count = getCharFrequecy(input, ch);
+			if (count > maxcount) {
+				maxcount = count;
+				maxchar = ch;
+
+			}
+		}
+		
+		return maxchar;
+	}
+
+	void getSecondMAx() {
+		String input = "aakanksha";
+
+		char maxchar = getMinFrequecy(input);
+		input = input.replace(Character.toString(maxchar), "");
+
+		char maxchar1 = getMinFrequecy(input);
+		input = input.replace(Character.toString(maxchar), "");
+		System.out.print(maxchar1);
+
+	}
+
+	public static void main(String[] args) {
+		Assignment60 assignment60 = new Assignment60();
+		assignment60.getSecondMAx();
+
+	}
+}
