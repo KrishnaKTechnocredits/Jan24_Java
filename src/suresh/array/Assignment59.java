1aa11660654f92c72bde4792deed7d7668e2d03b
sureshbabumc <sureshbabumc.@brillio.com>
2024-03-01 16:24:59 +0530

    suresh_Assign_59

---
+++ src/suresh/array/Assignment59.java
@@ -0,0 +1,46 @@
+/*
+ * Print the character with maximum frequency:
+input: TeeCccChH
+output: c->4"
+ */
+package suresh.array;
+
+public class Assignment59 {
+
+	int getCharFrequecy(String input, char ch) {
+		int count = 0;
+
+		for (int i = 0; i < input.length(); i++) {
+
+			char ch1 = input.charAt(i);
+			if (ch1 == ch) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	char getMaxFrequecy(String input) {
+		int maxcount = 0;
+		char maxchar = ' ';
+		String str = input.toLowerCase();
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			int count = getCharFrequecy(str, ch);
+			if (count > maxcount) {
+				maxcount = count;
+				maxchar = ch;
+
+			}
+
+		}
+		System.out.println(maxchar + "-->" + maxcount);
+
+		return maxchar;
+	}
+
+	public static void main(String[] args) {
+		Assignment59 assignment59 = new Assignment59();
+		assignment59.getMaxFrequecy("TeeCccChH");
+	}
+}
