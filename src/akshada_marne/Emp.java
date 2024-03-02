2bca8a37a98fd3e7d8469d360269aa30f5e7e078
Akshada Rajput <akshada.marne@gmail.com>
2024-03-02 13:23:56 +0530

    AkshadaR_Assignment68

---
+++ src/akshada_marne/Emp.java
@@ -0,0 +1,14 @@
+package akshada_marne;
+
+public class Emp extends Manager {
+	int y = 100;
+	int z = 200;
+
+	void m2() {
+		System.out.println("Employees m2");
+	}
+
+	void m3() {
+		System.out.println("Employee m3");
+	}
+}
