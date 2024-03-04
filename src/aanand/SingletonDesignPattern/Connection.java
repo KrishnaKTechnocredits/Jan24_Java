242465eed5361685867506c70b47ea7973dcc3ec
Anand Rathod <rathod.a86@gmail.com>
2024-03-04 17:51:58 +0530

    AnandR_Assignment_73

---
+++ src/aanand/SingletonDesignPattern/Connection.java
@@ -0,0 +1,18 @@
+package aanand.SingletonDesignPattern;
+
+public class Connection {
+
+	private static Connection con;
+
+	private Connection() {
+
+	}
+
+	static Connection getObject() {
+		if (con == null) {
+			con = new Connection();
+		}
+		return con;
+
+	}
+}
