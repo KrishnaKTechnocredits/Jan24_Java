133bbdf898d03199f1a53df8f7213dbafcc29146
Puja Poreddiwar <pujaporeddiwar123@gmail.com>
2024-03-05 18:44:19 +0530

    Puja_Assignment_74

---
+++ src/puja_poreddiwar/Assignment74BuilderClass.java
@@ -0,0 +1,15 @@
+package puja_poreddiwar;
+
+public class Assignment74BuilderClass {
+	
+	
+	public static void main(String[] args) {
+	Connection1 con1 = Connection1.getObject();
+	Connection1 con2 = Connection1.getObject();
+	Connection1 con3 = Connection1.getObject();
+	
+	System.out.println(con1);
+	System.out.println(con2);
+	System.out.println(con3);
+	}
+}
