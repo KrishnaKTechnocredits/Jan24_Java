91d560f0b940ca53ba32f238688ea0edf2d29a27
Gaurav Garggit config --global user.name "Gaurav Garg <gauravgarg9136@gmail.com>
2024-03-06 19:28:52 +0530

    Gaurav_Programming_test_73

---
+++ src/gaurav_garg/ClientClassAssignment73.java
@@ -0,0 +1,13 @@
+package gaurav_garg;
+
+public class ClientClassAssignment73 {
+
+	public static void main(String[] args) {
+		ConnectionclassAssignment73 con =ConnectionclassAssignment73.getMethod();
+		ConnectionclassAssignment73 con1 =ConnectionclassAssignment73.getMethod();
+		ConnectionclassAssignment73 con2 =ConnectionclassAssignment73.getMethod();
+		System.out.println(con);
+		System.out.println(con1);
+		System.out.println(con2);
+	}
+}
