def51072e5d21610c6aa43eedbbc7d8c732fc56f
madhurakulkarni009 <157051804+madhurakulkarni009@users.noreply.github.com>
2024-03-08 22:53:11 +0530

    Update ConnectionClassAssignment73.java

--- src/madhura_kulkarni/ConnectionClassAssignment73.java
+++ src/madhura_kulkarni/ConnectionClassAssignment73.java
@@ -3,6 +3,9 @@ package madhura_kulkarni;
 public class ConnectionClassAssignment73 {
 	private static ConnectionClassAssignment73 con = null;
 
+	ConnectionClassAssignment73 {
+	}
+	
 	public static ConnectionClassAssignment73 getObject() {
 		if (con == null)
 			con = new ConnectionClassAssignment73();
