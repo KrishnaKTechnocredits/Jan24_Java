5b1975574a57038a194d9693e947a557b553dc79
madhurakulkarni009 <157051804+madhurakulkarni009@users.noreply.github.com>
2024-03-08 22:53:51 +0530

    Update ConnectionClassAssignment73.java

--- src/madhura_kulkarni/ConnectionClassAssignment73.java
+++ src/madhura_kulkarni/ConnectionClassAssignment73.java
@@ -3,7 +3,7 @@ package madhura_kulkarni;
 public class ConnectionClassAssignment73 {
 	private static ConnectionClassAssignment73 con = null;
 
-	ConnectionClassAssignment73 {
+	private ConnectionClassAssignment73 {
 	}
 	
 	public static ConnectionClassAssignment73 getObject() {
