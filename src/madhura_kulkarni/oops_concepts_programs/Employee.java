ab68f18391727b879b35f03faee42a80ed1d7178
madhurakulkarni009 <madhura.kulkarni009@gmail.com>
2024-03-11 22:34:28 +0530

    src/madhura_kulkarni/oops_concepts_programs/Employee.java

---
+++ src/madhura_kulkarni/oops_concepts_programs/Employee.java
@@ -0,0 +1,15 @@
+package madhura_kulkarni.oops_concepts_programs;
+
+public class Employee extends Manager {
+		int empId = 456;
+		String empName = "Gargi";
+
+		void printId() {
+			System.out.println("Emp Id is " + managerId);
+		} 
+
+		void printName() {
+			System.out.println("Emp Name is " + empName);
+
+		}
+}
