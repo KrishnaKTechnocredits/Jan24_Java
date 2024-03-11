91e5b9b07a125c58a1d552e285a3b4fae7b0b9e0
Shridhar Kumbhar <shridhartechno@gmail.com>
2024-03-12 00:53:02 +0530

    Formatted

--- src/shridhar_k/Assignment79.java
+++ src/shridhar_k/Assignment79.java
@@ -1,28 +1,40 @@
+//1 
+//2 2 
+//3 3 3 
+//4 4 4 4 
+//5 5 5 5 5 
+//
+//* 
+//# # 
+//* * * 
+//# # # # 
+//* * * * *
+
 package shridhar_k;
 
 public class Assignment79 {
-	
+
 	void printNumColumnWise(int rowCount) {
-		for(int rowIndex =0;rowIndex<= rowCount;rowIndex++) {
-			for (int columnIndex=0;columnIndex<=rowIndex;columnIndex++) {
+		for (int rowIndex = 0; rowIndex <= rowCount; rowIndex++) {
+			for (int columnIndex = 0; columnIndex <= rowIndex; columnIndex++) {
 				System.out.print(rowIndex + 1 + " ");
 			}
 			System.out.println("");
 		}
 	}
-	
+
 	void printPattern(int rowCount) {
-		for(int rowIndex =0;rowIndex <= rowCount;rowIndex++) {
-			for (int columnIndex=0;columnIndex<=rowIndex;columnIndex++) {
-				if(rowIndex %2 ==0) {
+		for (int rowIndex = 0; rowIndex <= rowCount; rowIndex++) {
+			for (int columnIndex = 0; columnIndex <= rowIndex; columnIndex++) {
+				if (rowIndex % 2 == 0) {
 					System.out.print("* ");
-				}else
+				} else
 					System.out.print("# ");
 			}
 			System.out.println("");
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Assignment79 assignment79 = new Assignment79();
 		assignment79.printNumColumnWise(4);
