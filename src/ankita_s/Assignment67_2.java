d1e1f64a7ec3971f4dc3d4b25d3930b1a52d06ec
Ankita Sambhare <ankitasambhare94@gmail>
2024-03-12 15:26:21 +0530

    AnkitaS_Assignment_67

---
+++ src/ankita_s/Assignment67_2.java
@@ -0,0 +1,29 @@
+package ankita_s;
+
+public class Assignment67_2 {
+
+	String reverseStr(String str) {
+		String output = "";
+		for (int index = str.length() - 1; index >= 0; index--) {
+			char ch = str.charAt(index);
+			output += ch;
+		}
+		return output;
+	}
+
+	void reverseStrWithCapital1stLetter(String str) {
+		String[] strArr = str.split(" ");
+		String output = "";
+		for (int index = 0; index < strArr.length; index++) {
+			String revStr = reverseStr(strArr[index]).toLowerCase();
+			output += Character.toUpperCase(revStr.charAt(0)) + revStr.substring(1) + " ";
+		}
+		System.out.println("Output : " + output);
+	}
+
+	public static void main(String[] args) {
+		String str = "Hi Hello Maulik";
+		System.out.println("Input : " + str);
+		new Assignment67_2().reverseStrWithCapital1stLetter(str);
+	}
+}
