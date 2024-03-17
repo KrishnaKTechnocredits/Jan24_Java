f87a56220c0460b867a1c405bc19f48e77199ecb
Pallavi Raut <raut.pallavim162@gmail.com>
2024-03-17 19:34:33 +0530

    PallaviR_Assignment_66

---
+++ src/pallavi_raut/ReplaceDigitsWithSplChar_66.java
@@ -0,0 +1,23 @@
+/*Replace digits with a specific character ('*'):
+input: H2i H3el4lo P4un2e5
+output: H*i H*el*lo P*un*e*
+---------------------------------------------------------------*/
+package pallavi_raut;
+
+public class ReplaceDigitsWithSplChar_66 {
+
+	void toReplaceWithStar(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (Character.isDigit(ch)) {
+				str = str.replace(ch, '*');
+			}
+		}
+		System.out.println(str);
+	}
+
+	public static void main(String[] args) {
+		new ReplaceDigitsWithSplChar_66().toReplaceWithStar("H2i H3el4lo P4un2e5");
+	}
+
+}
