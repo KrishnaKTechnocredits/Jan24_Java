20c9bb84c9d6c7fb0827978a9552d2e174de99e2
Ankita Sambhare <ankitasambhare94@gmail>
2024-03-20 00:27:00 +0530

    AnkitaS_Assignment_81

---
+++ src/ankita_s/Assignment81.java
@@ -0,0 +1,31 @@
+package ankita_s;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Assignment81 {
+	int getVowelCount(String value) {
+		int count = 0;
+		for (int i = 0; i < value.length(); i++) {
+			if (value.charAt(i) == 'a' || value.charAt(i) == 'e' || value.charAt(i) == 'i' || value.charAt(i) == 'o'
+					|| value.charAt(i) == 'u') {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) throws IOException {
+		Assignment81 assignment81 = new Assignment81();
+		File f = new File(".//input.properties");
+		FileInputStream input = new FileInputStream(f);
+		Properties prop = new Properties();
+		prop.load(input);
+		String value = prop.getProperty("str");
+		int output = assignment81.getVowelCount(value);
+		System.out.println(value + " ---> " + output + " vowels.");
+
+	}
+}
