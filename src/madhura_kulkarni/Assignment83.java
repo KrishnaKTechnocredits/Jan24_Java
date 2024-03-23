f3446116eb014a2430260b8874b929663d30e116
madhurakulkarni009 <madhura.kulkarni009@gmail.com>
2024-03-23 16:27:30 +0530

    src/madhura_kulkarni/Assignment83.java

---
+++ src/madhura_kulkarni/Assignment83.java
@@ -0,0 +1,35 @@
+/*Assignment-83 : 21st March'2024
+I__am___in_pune____
+
+output : I -> 2
+         am -> 3
+		 in -> 1
+         pune -> 4	*/
+package madhura_kulkarni;
+
+public class Assignment83 {
+	void getUnderscoreCountWord(String str) {
+		str = str + 'a';
+		String word = "";
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (Character.isLetter(ch)) {
+				word = word + ch;
+			} else if ((ch == '_' && (Character.isLetter(str.charAt(i + 1))))) {
+				count++;
+				System.out.println(word + " --> " + count);
+				word = "";
+				count = 0;
+			} else if (ch == '_') {
+				count++;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String str = "I__am___in_pune____";
+		new Assignment83().getUnderscoreCountWord(str);
+	}
+
+}
