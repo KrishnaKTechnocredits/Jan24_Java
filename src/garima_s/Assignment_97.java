0c36c7d1fcfaca27642d081d02e82cfe2915a3b9
Garima <garimasinghal2285@gmail.com>
2024-03-27 14:05:38 +0530

    GarimaS_Assignment_97

---
+++ src/garima_s/Assignment_97.java
@@ -0,0 +1,43 @@
+package garima_s;
+
+public class Assignment_97 {
+
+	void getArmStrongNumber(int[] arr) {
+		System.out.print("Armstrong Numbers : ");
+		for (int i = 0; i < arr.length; i++) {
+			if (isArmStrongNumber(arr[i])) {
+				System.out.println(arr[i]);
+			}
+		}
+	}
+
+	boolean isArmStrongNumber(int num) {
+		int rem = 0;
+		int sum = 0;
+		int originalNum = num;
+		int digit = getDigits(num);
+		while (num > 0) {
+			rem = num % 10;
+			num = num / 10;
+			sum += Math.pow(rem, digit);
+		}
+		if (sum == originalNum) {
+			return true;
+		}
+		return false;
+	}
+
+	int getDigits(int number) {
+		int count = 0;
+		while (number > 0) {
+			number = number / 10;
+			count++;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 153, 27, 1634, 99 };
+		new Assignment_97().getArmStrongNumber(arr);
+	}
+}
