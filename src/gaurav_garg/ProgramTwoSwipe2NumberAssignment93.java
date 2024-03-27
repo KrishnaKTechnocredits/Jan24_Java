250ffe7b7d0c37bdf09f4ac768f495c12d8f126b
Gaurav Garggit config --global user.name "Gaurav Garg <gauravgarg9136@gmail.com>
2024-03-27 15:19:42 +0530

    Gaurav_Assignment_93

---
+++ src/gaurav_garg/ProgramTwoSwipe2NumberAssignment93.java
@@ -0,0 +1,50 @@
+package gaurav_garg;
+
+public class ProgramTwoSwipe2NumberAssignment93 {
+
+	void swipeTwoNumbweWithThirdVariable(int a, int b) {
+		System.out.println("Using Third Variable");
+		System.out.println("Number before Number Swipped -: " + a + " " + b);
+		int c = 0;
+		c = b;
+		b = a;
+		a = c;
+		System.out.println("Number After Number Swipped -: " + a + " " + b);
+	}
+
+	void swipeTwoNumbweWithoutThirdVariable(int a, int b) {
+		System.out.println("Using Without Third Variable");
+		System.out.println("Number before Number Swipped -: " + a + " " + b);
+		b = a + b;
+		a = b - a;
+		b = b - a;
+		System.out.println("Number After Number Swipped -: " + a + " " + b);
+
+	}
+
+	void swipeTwStringWithThirdVariable(String a, String b) {
+		System.out.println("Using Third Variable");
+		System.out.println("Number before Number Swipped -: " + a + " " + b);
+		String c = null;
+		c = b;
+		b = a;
+		a = c;
+		System.out.println("Number After Number Swipped -: " + a + " " + b);
+	}
+
+	void swipeTwStringWithoutThirdVariable(String a, String b) {
+		System.out.println("Using Third Variable");
+		System.out.println("Number before Number Swipped -: " + a + " " + b);
+		b = a + " " + b;
+		a = b.split(" ")[1];
+		b = b.split(" ")[0];
+		System.out.println("Number After Number Swipped -: " + a + " " + b);
+	}
+
+	public static void main(String[] args) {
+		new ProgramTwoSwipe2NumberAssignment93().swipeTwoNumbweWithThirdVariable(60, 20);
+		new ProgramTwoSwipe2NumberAssignment93().swipeTwoNumbweWithoutThirdVariable(60, 20);
+		new ProgramTwoSwipe2NumberAssignment93().swipeTwStringWithThirdVariable("Gaurav", "Ram");
+		new ProgramTwoSwipe2NumberAssignment93().swipeTwStringWithoutThirdVariable("Gaurav", "Ram");
+	}
+}
