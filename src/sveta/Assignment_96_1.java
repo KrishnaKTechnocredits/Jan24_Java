383be08cb23d2ce2ef574ce56372f2a792215cae
sveta.kanani <sveta.kanani123@gmail.com>
2024-03-29 11:07:03 +0530

    Sveta_Assignment_96

---
+++ src/sveta/Assignment_96_1.java
@@ -0,0 +1,26 @@
+/*2. Print all the numbers of Fibonacci series where in series number should not cross 100.*/
+package sveta;
+
+public class Assignment_96_1 {
+
+	public void findFebonacci(int no) {
+		int no1=0;
+		int no2 =1;
+		int temp=0;
+		
+		for	(int i=0;i<no;i++) {
+			if(temp<=100) {
+				System.out.println(no1);
+			}else {
+			}
+			temp = no1+no2;
+			no1=no2;
+			no2=temp;
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Assignment_96_1().findFebonacci(10);
+	}
+
+}
