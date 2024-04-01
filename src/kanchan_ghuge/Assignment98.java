30031e3096f5fb2b83c762f14ba5f3ba6e7308fd
Kanchan Ghuge <kg5995976@gmail.com>
2024-04-02 04:55:08 +0530

    KanchanG_Assignment_98

---
+++ src/kanchan_ghuge/Assignment98.java
@@ -0,0 +1,41 @@
+
+/*
+ 	
+Using Map, find out frequency of each special characters.
+
+String name = "h#i#ra_l_tec#h!n!o!"
+output : # -> 3
+         _ -> 2
+                 ! -> 3 */
+package kanchan_ghuge;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+class Assignment98 {
+
+	void getFreqOfSpecialChar(String input) {
+		char[] arr = input.toCharArray();
+		Map<Character, Integer> charMap = new HashMap<Character, Integer>();
+		for (Character ch : arr) {
+			if (charMap.containsKey(ch)) {
+				int count = charMap.get(ch);
+				charMap.put(ch, count + 1);
+			} else
+				charMap.put(ch, 1);
+		}
+		Set<Character> charKeys = charMap.keySet();
+		for (char ch1 : charKeys) {
+			if (ch1 == '#' || ch1 == '_' || ch1 == '!') {
+				int value = charMap.get(ch1);
+				System.out.println(ch1 + "-->" + value);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String name = "h#i#ra_l_tec#h!n!o!";
+		new Assignment98().getFreqOfSpecialChar(name);
+	}
+}
